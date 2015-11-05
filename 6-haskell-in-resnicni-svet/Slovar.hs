8c3fbf3be593bd312ced6c79a9d5bb6f79f3f88f
Matija Pretnar <matija@pretnar.info>
2015-11-05 17:10:49 +0100

    Primer modula in IO-ja

---
+++ 6-haskell-in-resnicni-svet/Slovar.hs
@@ -0,0 +1,52 @@
+--module Slovar
+--  ( Slovar
+--  , dodaj
+--  , poisci
+--  , prazen
+--  ) where
+
+--    data Slovar k v = Slovar [(k, v)] deriving Show
+
+--    dodaj :: Slovar k v -> k -> v -> Slovar k v
+--    dodaj (Slovar d) k v = Slovar ((k, v) : d)
+
+--    poisci :: Eq k => Slovar k v -> k -> Maybe v
+--    poisci (Slovar []) _ = Nothing
+--    poisci (Slovar ((k,v):d)) k'
+--        | k == k' = Just v
+--        | otherwise = poisci (Slovar d) k'
+
+--    prazen :: Slovar k v
+--    prazen = Slovar []
+
+
+
+module Slovar
+  ( Slovar
+  , dodaj
+  , poisci
+  , prazen
+  ) where
+
+    data Slovar k v = Slovar [(k, v)] deriving Show
+
+    dodaj :: Ord k => Slovar k v -> k -> v -> Slovar k v
+    dodaj (Slovar d) k v = Slovar (dodaj' d k v)
+      where
+        dodaj' [] k v = [(k, v)]
+        dodaj' ((k, v):d) k' v'
+            | k < k' = (k, v) : dodaj' d k' v'
+            | k == k' = (k', v') : d
+            | otherwise = (k', v') : (k, v) : d
+
+    poisci :: Ord k => Slovar k v -> k -> Maybe v
+    poisci (Slovar d) k = poisci' d k
+      where
+        poisci' [] _ = Nothing
+        poisci' ((k,v):d) k'
+            | k < k' = poisci' d k
+            | k == k' = Just v
+            | otherwise = Nothing
+
+    prazen :: Slovar k v
+    prazen = Slovar []
---
+++ 6-haskell-in-resnicni-svet/stetje_besed.hs
@@ -0,0 +1,23 @@
+import Slovar
+import Test.QuickCheck
+
+s = prazen
+
+dodajCrke :: Slovar Char Integer -> String -> Slovar Char Integer
+dodajCrke d [] = d
+dodajCrke d (crka:vrstica) =
+    case poisci d crka of
+        Nothing -> dodajCrke (dodaj d crka 1) vrstica
+        Just n -> dodajCrke (dodaj d crka (n + 1)) vrstica
+
+dodajaj d =
+    do
+        vrstica <- getLine
+        if null vrstica then
+            return d
+        else
+            dodajaj $ dodajCrke d vrstica
+
+main = do
+    d <- dodajaj prazen
+    print d
