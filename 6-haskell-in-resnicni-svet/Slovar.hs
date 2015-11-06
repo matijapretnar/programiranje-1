37e10667561e50aff216c293f8582dab1867dde9
Matija Pretnar <matija@pretnar.info>
2015-11-06 13:49:09 +0100

    Datoteke, napisane na predavanjih

--- 6-haskell-in-resnicni-svet/Slovar.hs
+++ 6-haskell-in-resnicni-svet/Slovar.hs
@@ -1,61 +1,39 @@
 module Slovar
-  ( Slovar
-  , dodaj
-  , poisci
-  , prazen
-  ) where
-
-    data Slovar k v = Slovar [(k, v)] deriving Show
-
-    dodaj :: Slovar k v -> k -> v -> Slovar k v
-    dodaj (Slovar d) k v = Slovar ((k, v) : d)
-
-    poisci :: Eq k => Slovar k v -> k -> Maybe v
-    poisci (Slovar []) _ = Nothing
-    poisci (Slovar ((k,v):d)) k'
-        | k == k' = Just v
-        | otherwise = poisci (Slovar d) k'
-
-    prazen :: Slovar k v
-    prazen = Slovar []
-
-
-
-module Slovar
-  ( Slovar
-  , dodaj
-  , poisci
-  , prazen
-  , moj
-  ) where
+    ( Slovar
+    , prazen
+    , poisci
+    , dodaj
+    )where
 
     import Test.QuickCheck
     import Control.Monad
 
-    instance  (Arbitrary k, Arbitrary v) => Arbitrary (Slovar k v)  where
-       arbitrary = liftM Slovar arbitrary
-
     data Slovar k v = Slovar [(k, v)] deriving Show
 
-    dodaj :: Ord k => Slovar k v -> k -> v -> Slovar k v
-    dodaj (Slovar d) k v = Slovar (dodaj' d k v)
-      where
-        dodaj' [] k v = [(k, v)]
-        dodaj' ((k, v):d) k' v'
-            | k < k' = (k, v) : dodaj' d k' v'
-            | k == k' = (k', v') : d
-            | otherwise = (k', v') : (k, v) : d
-
-    poisci :: Ord k => Slovar k v -> k -> Maybe v
-    poisci (Slovar d) k = poisci' d k
-      where
-        poisci' [] _ = Nothing
-        poisci' ((k,v):d) k'
-            | k < k' = poisci' d k'
-            | k == k' = Just v
-            | otherwise = Nothing
+    instance (Arbitrary k, Arbitrary v) => Arbitrary (Slovar k v) where
+        arbitrary = liftM Slovar arbitrary
 
     prazen :: Slovar k v
     prazen = Slovar []
 
-    moj = Slovar [('Z',-1),('\136',-5),('|',-1),('^',-3)]
+    poisci :: (Eq k) => Slovar k v -> k -> Maybe v
+    poisci (Slovar d) k = poisci' d k
+      where
+        poisci' [] _ = Nothing
+        poisci' ((k,v):d) k'
+            | k' == k = Just v
+            | otherwise = poisci' d k'
+
+    --zlij :: Slovar k v -> Slovar k v -> Slovar k v
+    --izSeznama :: [(k, v)] -> Slovar k v
+    --dodajSeznam :: Slovar k v -> [(k, v)] -> Slovar k v
+    --dodaj :: Slovar k v -> (k, v) -> Slovar k v
+    dodaj :: (Eq k) => Slovar k v -> k -> v -> Slovar k v
+    dodaj (Slovar d) k v = Slovar (dodaj' d k v)
+      where
+        -- Domača naloga: napiši boljšo implementacijo
+        dodaj' [] k v = [(k, v)]
+        dodaj' ((k,v):d) k' v'
+            | k' == k = (k', v'):d
+            | otherwise = (k,v') : dodaj' d k' v'
+
--- 6-haskell-in-resnicni-svet/SlovarTest.hs
+++ 6-haskell-in-resnicni-svet/SlovarTest.hs
@@ -1,20 +1,25 @@
-import Test.QuickCheck
 import Slovar
+import Test.QuickCheck
 
---prop_poisciPrazen :: (Eq k) => k -> Bool
---prop_poisciPrazen k =
---    poisci prazen k == Nothing
+dodajCrke :: Slovar Char Int -> String -> Slovar Char Int
+dodajCrke d "" = d
+dodajCrke d (crka:niz) = dodajCrke (dodajCrka d crka) niz
+  where
+    dodajCrka d crka =
+        case poisci d crka of
+            Nothing -> dodaj d crka 1
+            Just n -> dodaj d crka (1 + n)
 
---prop_poisciDodajPrazen :: (Ord k, Eq v) => k -> v -> Bool
---prop_poisciDodajPrazen k v =
---    poisci (dodaj prazen k v) k == Just v
+prop_poisciDodaj :: Slovar Char Int -> Char -> Int -> Bool
+prop_poisciDodaj d k v = poisci (dodaj d k v) k == Just v
 
-prop_poisciDodaj :: (Ord k, Eq v) => Slovar k v -> k -> v -> Bool
-prop_poisciDodaj d k v =
-    poisci (dodaj d k v) k == Just v
+prop_poisciDodaj2 :: Slovar Char Int -> Char -> Char -> Int -> Int -> Bool
+prop_poisciDodaj2 d k k' v v' = poisci (dodaj (dodaj d k v) k' v') k == Just v
+
+--Slovar [('0',-1)]
+--'a'
+--0
 
 main = do
-    --quickCheck (prop_poisciPrazen :: Char -> Bool)
-    --quickCheck (prop_poisciPrazen :: Int -> Bool)
-    quickCheck (prop_poisciDodaj :: Slovar Int Char -> Int -> Char -> Bool)
-    quickCheck (prop_poisciDodaj :: Slovar Char Int -> Char -> Int -> Bool)
+    quickCheck prop_poisciDodaj2
+    --print (dodajCrke prazen "abrakadabra")
