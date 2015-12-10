c6a90a254e9730b2fef09fa2f295ad16a99be2d5
Matija Pretnar <matija@pretnar.info>
2015-12-10 14:14:51 +0100

    Datoteke za deli & vladaj

---
+++ 11-deli-in-vladaj/datoteke/hitrourejanje.py
@@ -0,0 +1,43 @@
+def quicksort(sez):
+    if len(sez) <= 1:
+        return sez
+    else:
+        pivot = sez[0]
+        manjsi = []
+        vecji = []
+        for x in sez[1:]:
+            if x <= pivot:
+                manjsi.append(x)
+            else:
+                vecji.append(x)
+        return quicksort(manjsi) + [pivot] + quicksort(vecji)
+
+
+def premeci(sez, i, j):
+    assert j - i > 2
+    pivot = i
+    i += 1
+    j -= 1
+    while i < j:
+        while i < j and sez[i] <= sez[pivot]:
+            i += 1
+        while i < j and sez[j] > sez[pivot]:
+            j -= 1
+        sez[i], sez[j] = sez[j], sez[i]
+    if sez[i] <= sez[pivot]:
+        novi_pivot = i
+    else:
+        novi_pivot = i - 1
+    sez[pivot], sez[novi_pivot] = sez[novi_pivot], sez[pivot]
+    return novi_pivot
+
+
+def quicksort_na_mestu(sez, i=0, j=None):
+    if j is None:
+        j = len(sez)
+    if j - i <= 2:
+        return
+    else:
+        pivot = premeci(sez, i, j)
+        quicksort_na_mestu(sez, i, pivot)
+        quicksort_na_mestu(sez, pivot + 1, j)
--- 11-deli-in-vladaj/datoteke/potence.py
+++ 11-deli-in-vladaj/datoteke/potence.py
@@ -1,22 +1,48 @@
-def pocasni_potenciraj(a, n):
+def pocasni_potenciraj_rek(a, n):
     if n == 0:
         return 1
     else:
-        return a * pocasni_potenciraj(a, n - 1)
+        return a * pocasni_potenciraj_rek(a, n - 1)
+
+
+def pocasni_potenciraj_imp(a, n):
+    potenca = 1
+    for _ in range(n):
+        potenca *= a
+    return potenca
 
 
 def potenciraj(a, n):
     if n == 0:
         return 1
     elif n % 2 == 0:
-        return potenciraj(a, n // 2) ** 2
+        return potenciraj(a ** 2, n // 2)
     else:
-        return a * potenciraj(a, n // 2) ** 2
+        return a * potenciraj(a ** 2, n // 2)
 
 
-import timeit
+def zmnozi(a, b):
+    return [[a[0][0] * b[0][0] + a[0][1] * b[1][0],
+             a[0][0] * b[0][1] + a[0][1] * b[1][1]],
+            [a[1][0] * b[0][0] + a[1][1] * b[1][0],
+             a[1][0] * b[0][1] + a[1][1] * b[1][1]]]
 
-print(timeit.timeit('potenciraj(2, 500)',
-      'from __main__ import potenciraj', number=5000))
-print(timeit.timeit('pocasni_potenciraj(2, 500)',
-      'from __main__ import pocasni_potenciraj', number=5000))
+
+def potenciraj_matriko(a, n):
+    if n == 0:
+        return [[1, 0], [0, 1]]
+    elif n % 2 == 0:
+        return potenciraj_matriko(zmnozi(a, a), n // 2)
+    else:
+        return zmnozi(a, potenciraj_matriko(zmnozi(a, a), n // 2))
+
+
+def pocasni_fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
+
+
+def fibonacci(n):
+    return potenciraj_matriko([[1, 1], [1, 0]], n - 1)[0][0]
