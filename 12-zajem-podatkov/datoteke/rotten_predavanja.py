2e5d674d578ea3f0bd450d5e53cd7ed789d100fc
Matija Pretnar <matija@pretnar.info>
2016-01-07 10:27:56 +0100

    Ločil datoteke na tiste s predavanj in tiste za projekt

--- 12-zajem-podatkov/datoteke/rotten_predavanja.py
+++ 12-zajem-podatkov/datoteke/rotten_predavanja.py
@@ -1,11 +1,11 @@
 from utils import *
 import re
 
-shrani('http://www.rottentomatoes.com/top/bestofrt/top_100_action__adventure_movies/', 'podatki/akcije.html')
+shrani('http://www.rottentomatoes.com/top/bestofrt/top_100_action__adventure_movies/', 'akcije.html')
 
 vzorec_filma = re.compile(
     r'<tr> <td class="bold">(?P<mesto>\d+)\..*?(?P<ocena>\d+)%.*?">(?P<naslov>.*?) \((?P<leto>\d{4})'
 )
 
-for ujemanje in re.finditer(vzorec_filma, vsebina_datoteke('podatki/akcije.html')):
+for ujemanje in re.finditer(vzorec_filma, vsebina_datoteke('akcije.html')):
     print(ujemanje.groupdict())
---
+++ projekt/utils.py
@@ -0,0 +1,34 @@
+import os
+import requests
+import sys
+
+
+def shrani(url, ime_datoteke, vsili_prenos=False):
+    '''Vsebino strani na danem naslovu shrani v datoteko z danim imenom.'''
+    try:
+        print('Shranjujem {}...'.format(url), end='')
+        sys.stdout.flush()
+        if os.path.isfile(ime_datoteke) and not vsili_prenos:
+            print('shranjeno že od prej!')
+            return
+        r = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        print('stran ne obstaja!')
+    imenik = os.path.dirname(ime_datoteke)
+    if imenik:
+        os.makedirs(imenik, exist_ok=True)
+    with open(ime_datoteke, 'w') as datoteka:
+        datoteka.write(r.text)
+        print('shranjeno!')
+
+
+def vsebina_datoteke(ime_datoteke):
+    '''Vrne niz z vsebino datoteke z danim imenom.'''
+    with open(ime_datoteke) as datoteka:
+        vsebina = datoteka.read()
+    return vsebina
+
+
+def datoteke(imenik):
+    '''Vrne imena vseh datotek v danem imeniku skupaj z imenom imenika.'''
+    return [os.path.join(imenik, datoteka) for datoteka in os.listdir(imenik)]
