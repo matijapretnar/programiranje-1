62d6ac39e7a009fb3cbb9ef8e543719ff740073b
Matija Pretnar <matija@pretnar.info>
2016-10-08 16:46:21 +0200

    Dodal skripto, ki dela CSV s filmi

---
+++ ap-2-urejanje-podatkov/predavanja/orodja.py
@@ -0,0 +1,51 @@
+import csv
+import os
+import requests
+import sys
+
+
+def pripravi_imenik(ime_datoteke):
+    '''Če še ne obstaja, pripravi prazen imenik za dano datoteko.'''
+    imenik = os.path.dirname(ime_datoteke)
+    if imenik:
+        os.makedirs(imenik, exist_ok=True)
+
+
+def shrani(url, ime_datoteke, vsili_prenos=False):
+    '''Vsebino strani na danem naslovu shrani v datoteko z danim imenom.'''
+    try:
+        print('Shranjujem {}...'.format(url), end='')
+        sys.stdout.flush()
+        if os.path.isfile(ime_datoteke) and not vsili_prenos:
+            print('shranjeno že od prej!')
+            return
+        r = requests.get(url, headers={'Accept-Language': 'en'})
+    except requests.exceptions.ConnectionError:
+        print('stran ne obstaja!')
+    else:
+        pripravi_imenik(ime_datoteke)
+        with open(ime_datoteke, 'w') as datoteka:
+            datoteka.write(r.text)
+            print('shranjeno!')
+
+
+def vsebina_datoteke(ime_datoteke):
+    '''Vrne niz z vsebino datoteke z danim imenom.'''
+    with open(ime_datoteke) as datoteka:
+        vsebina = datoteka.read()
+    return vsebina
+
+
+def datoteke(imenik):
+    '''Vrne imena vseh datotek v danem imeniku skupaj z imenom imenika.'''
+    return [os.path.join(imenik, datoteka) for datoteka in os.listdir(imenik)]
+
+
+def zapisi_tabelo(slovarji, imena_polj, ime_datoteke):
+    '''Iz seznama slovarjev ustvari CSV datoteko z glavo.'''
+    pripravi_imenik(ime_datoteke)
+    with open(ime_datoteke, 'w') as csv_dat:
+        writer = csv.DictWriter(csv_dat, fieldnames=imena_polj)
+        writer.writeheader()
+        for slovar in slovarji:
+            writer.writerow(slovar)
---
+++ ap-2-urejanje-podatkov/predavanja/ustvari_csv.py
@@ -0,0 +1,42 @@
+import re
+import orodja
+
+
+def zajemi_imdb():
+    osnovni_naslov = 'http://www.imdb.com/search/title'
+    parametri = 'sort=num_votes,desc&title_type=feature&num_votes=25000,'
+    for stran in range(1, 51):
+        naslov = '{}?{}&page={}'.format(osnovni_naslov, parametri, stran)
+        ime_datoteke = 'imdb/{:02}.html'.format(stran)
+        orodja.shrani(naslov, ime_datoteke)
+
+
+def pripravi_imdb():
+    regex_filma = re.compile(
+        r'href="/title/tt(?P<id>\d+)/\?ref_=adv_li_tt"[^>]*?'
+        r'>(?P<naslov>.*?)</a>.*?'
+        r'lister-item-year text-muted unbold">.*?\((?P<leto>\d{4})\)</span>.*?'
+        r'(certificate">(?P<certifikat>.*?)</.*?)?'
+        r'runtime">(?P<runtime>.*?) min</.*?'
+        r'<strong>(?P<ocena>.+?)</strong>.*?'
+        r'<p class="text-muted">(?P<opis>.+?)<.*?'
+        r'Directors?:.*?<a href=.*?>(?P<reziser>.+?)</a>.*?',
+        flags=re.DOTALL
+    )
+
+    filmi = []
+    for i, html_datoteka in enumerate(orodja.datoteke('imdb/')):
+        for j, film in enumerate(re.finditer(regex_filma, orodja.vsebina_datoteke(html_datoteka))):
+            podatki = film.groupdict()
+            podatki['id'] = int(podatki['id'])
+            podatki['runtime'] = int(podatki['runtime'])
+            podatki['leto'] = int(podatki['leto'])
+            podatki['opis'] = podatki['opis'].strip()
+            podatki['ocena'] = float(podatki['ocena'])
+            filmi.append(podatki)
+
+    orodja.zapisi_tabelo(filmi, ['id', 'naslov', 'leto', 'reziser', 'certifikat', 'runtime', 'ocena', 'opis'], 'filmi.csv')
+
+
+zajemi_imdb()
+pripravi_imdb()
