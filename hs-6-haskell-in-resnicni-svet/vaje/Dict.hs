d121184e31d76633a952d4892afd7a3801531a03
Philipp Haselwarter <philipp@haselwarter.org>
2016-12-11 21:59:37 +0100

    (hs-6) Add generator for search trees, tests

--- hs-6-haskell-in-resnicni-svet/vaje/Dict.hs
+++ hs-6-haskell-in-resnicni-svet/vaje/Dict.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+-- We implement dictionaries using naïve binary search trees.
 module Dict
   ( Dict
   , empty
@@ -5,6 +8,7 @@ module Dict
   , search
   , remove
   , fromList
+  , internalTests
   )
   where
 
@@ -56,13 +60,55 @@ remove k (Node l a@(k',_) r) =
 fromList :: Ord k => [(k,v)] -> Dict k v
 fromList = foldl (flip $ uncurry add) empty
 
+depth :: Tree a -> Integer
+depth Leaf = 0
+depth (Node l _ r) = 1 + max (depth l) (depth r)
+
 
 -- We provide a generator for random trees for QuickCheck.
-instance (Arbitrary a) => Arbitrary (Tree a) where
-  arbitrary = sized tree'
-    where tree' n = case compare n 0 of
+arbitraryTree :: (Arbitrary a) => Gen (Tree a)
+arbitraryTree = sized genTree
+    where genTree n = case compare n 0 of
             LT -> return Leaf
             EQ -> return Leaf
             GT -> oneof [return Leaf,
                          liftM3 Node subtree arbitrary subtree]
-              where subtree = tree' (n `div` 2)
+              where subtree = genTree (n `div` 2)
+
+-- ...but we don't use it, because it does not generate search trees.
+-- instance Arbitrary a => Arbitrary (Tree a) where
+--   arbitrary = arbitraryTree
+
+-- Instead, here's a generator for search trees.
+
+-- Note that the reliability and significance of all the tests that use a
+-- generator G rely on the correctness of G. Here, we rely on [fromList] and
+-- thus on [add]. We could instead write a potentially complex generator.
+genSearchTree :: (Ord k, Arbitrary k, Arbitrary v) => Gen (Dict k v)
+genSearchTree = liftM fromList arbitrary
+
+instance (Ord k, Arbitrary k, Arbitrary v) => Arbitrary (Dict k v) where
+    arbitrary = genSearchTree
+
+isSorted :: (Ord k) => Dict k v -> Bool
+isSorted d = isSortedBetween Nothing d Nothing where
+  isSortedBetween _ Leaf _ = True
+  isSortedBetween minK (Node l (k, _) r) maxK =
+    minK <=? Just k && Just k <=? maxK &&
+    isSortedBetween minK l (Just k) &&
+    isSortedBetween (Just k) r maxK
+  Nothing <=? _ = True
+  _ <=? Nothing = True
+  Just x <=? Just y = x <= y
+
+-- This should always hold if the generator uses [add] internally.
+prop_isSortedAdd :: Ord k => Dict k v -> k -> v -> Property
+prop_isSortedAdd d k v =
+    collect (depth d) $ collect (isSorted d) $ (isSorted d) ==> (isSorted (add k v d))
+
+internalTests :: IO ()
+internalTests = do
+  quickCheck (isSorted :: Dict Integer String -> Bool)
+  quickCheck (isSorted :: Dict String Integer -> Bool)
+  quickCheck (prop_isSortedAdd :: Dict Integer String -> Integer -> String -> Property)
+  quickCheck (prop_isSortedAdd :: Dict String Integer -> String -> Integer -> Property)
