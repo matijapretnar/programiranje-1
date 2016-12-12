0db3e255a73e1f70caeda90056c577cfb11f8c79
Philipp Haselwarter <philipp@haselwarter.org>
2016-12-12 10:57:47 +0100

    Phonebook exercises

--- hs-6-haskell-in-resnicni-svet/vaje/Phonebook.hs
+++ hs-6-haskell-in-resnicni-svet/vaje/Phonebook.hs
@@ -12,59 +12,28 @@ data Command = Op Operation | UnknownCmd String | Quit
 
 -- [readName] asks the user to input a name and reads it from the standard input
 readName :: IO String
-readName = do
-  putStr "Enter name: "
-  getLine
+readName = undefined
 
 -- [readName] asks the user to input a phone number and reads it from the standard input
 readNumber :: IO String
-readNumber = do
-  putStr "Enter number: "
-  getLine
+readNumber = undefined
 
 -- [readCommand] asks the user to input one of the available commads and reads it
 readCommand :: IO Command
 readCommand = do
   putStr "Choose command (add,search,remove,print,quit) : "
-  cmd <- getLine
-  return $ case cmd of
-    "add" -> Op Add
-    "search" -> Op Search
-    "remove" -> Op Remove
-    "print" -> Op PrintAll
-    "quit" -> Quit
-    _ -> UnknownCmd cmd
+  undefined
 
 -- [runOperation pb op] runs the operation [op] on the phone book [pb]
 runOperation :: Phonebook -> Operation -> IO Phonebook
-runOperation pb Search = do name <- readName
-                            case search name pb of
-                              Nothing -> putStrLn $ "Name \"" ++ name ++ "\" not found."
-                              Just nb -> putStrLn $ "Found number: " ++ nb
-                            return pb
-runOperation pb Add = do name <- readName
-                         num <- readNumber
-                         return $ add name num pb
-runOperation pb Remove = do name <- readName
-                            return $ remove name pb
-runOperation pb PrintAll = do putStrLn $ show pb
-                              return pb
+runOperation = undefined
 
 -- [interactionLoop pb] reads a [Command] and performs the corresponding
 -- action: an operation is executed and an unknown command is signalled to the
 -- user by writing a warning. Until a [Quit] is encountered, at which point the
 -- function returns.
 interactionLoop :: Phonebook -> IO ()
-interactionLoop pb = do
-  step <- readCommand
-  case step of
-    UnknownCmd cmd -> do
-      putStrLn $ "Unknown command " ++ cmd
-      interactionLoop pb
-    Quit -> return ()
-    Op op -> do
-      pb' <- runOperation pb op
-      interactionLoop pb'
+interactionLoop = undefined
 
 main :: IO ()
 main = interactionLoop empty
