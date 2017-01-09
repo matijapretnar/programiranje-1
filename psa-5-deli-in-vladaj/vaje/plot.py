0525e4b16d5adbc297f08f70b62b510c2e68afd5
Philipp Haselwarter <philipp@haselwarter.org>
2017-01-09 09:57:52 +0100

    (psa-5) Add insertionsort

--- psa-5-deli-in-vladaj/vaje/plot.py
+++ psa-5-deli-in-vladaj/vaje/plot.py
@@ -9,13 +9,16 @@ def time_quicksort_raw():
     return time_algo_raw("quicksort", "gen_lists")
 def time_builtin_sort_raw():
     return time_algo_raw("builtin_sort", "gen_lists")
+def time_insertionsort_raw():
+    return time_algo_raw("insertionsort", "gen_lists")
 
 def time_quickselect_raw():
     return time_algo_raw("quickselect_k", "gen_lists_ks")
 
 
 def plot_times():
-    for f in [time_builtin_sort_raw, time_qsrt_raw, time_quicksort_raw, time_quickselect_raw]:
+    for f in [time_builtin_sort_raw, time_insertionsort_raw,
+              time_qsrt_raw, time_quicksort_raw, time_quickselect_raw]:
         label, lengths_times = f()
         lengths, times = [], []
         for l_t in lengths_times:
