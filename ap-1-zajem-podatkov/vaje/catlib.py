53542ec1da7b0a15abaa595c019b5277a3c110e0
Philipp G. Haselwarter <philipp@haselwarter.org>
2017-10-13 15:42:51 +0200

    Start solving the bolha cat library

--- ap-1-zajem-podatkov/vaje/catlib.py
+++ ap-1-zajem-podatkov/vaje/catlib.py
@@ -9,25 +9,33 @@ import csv
 ########################################################################
 
 # define the URL of the main page of the bolha cats listing
-cats_frontpage_url = "TODO"
+cats_frontpage_url = 'http://www.bolha.com/zivali/male-zivali/macke/'
 # the directory to which we save our data
-cat_directory = "TODO"
+cat_directory = 'my_cats'
 # the filename we use to save the frontpage
-frontpage_filename = "TODO"
+frontpage_filename = "cats_fp.html"
 # the filename for the CSV file for the extracted data
-csv_filename = "TODO"
+csv_filename = "cats.csv"
 
 def download_url_to_string(url):
     '''This function takes a URL as argument and tries to download it
     using requests. Upon success, it returns the page contents as string.'''
-    TODO
+    try:
+        r = requests.get(url)
+    except requests.exceptions.ConnectionError:
+        print("failed to connect to url " + url)
+        return
+    if r.status_code == requests.codes.ok:
+        return r.text
+    print("failed to download url " + url)
+    return
 
 
 def save_string_to_file(text, directory, filename):
     '''Write "text" to the file "filename" located in directory "directory",
     creating "directory" if necessary. If "directory" is the empty string, use
     the current directory.'''
-    os.makedir(directory, exists_ok=True)
+    os.makedirs(directory, exist_ok=True)
     path = os.path.join(directory, filename)
     with open(path, 'w') as file_out:
         file_out.write(text)
@@ -35,10 +43,11 @@ def save_string_to_file(text, directory, filename):
 
 
 # Define a function that downloads the frontpage and saves it to a file.
-def undefined( TODO ):
-    '''TODO'''
-    TODO
-
+def save_frontpage():
+    '''Save "cats_frontpage_url" to the file "cat_directory"/"frontpage_filename"'''
+    text = download_url_to_string(cats_frontpage_url)
+    save_string_to_file(text, cat_directory, frontpage_filename)
+    return None
 
 
 ########################################################################
@@ -48,9 +57,9 @@ def undefined( TODO ):
 def read_file_to_string(directory, filename):
     '''Return the contents of the file "directory"/"filename" as a string.
     '''
-    path = os.path.join( TODO )
+    path = os.path.join(directory, filename)
     with open(path, 'r') as file_in:
-        TODO
+        return file_in.read()
 
 
 # Define a function that takes a webpage as a string and splits it into
@@ -58,17 +67,24 @@ def read_file_to_string(directory, filename):
 # function will use a regular expression that delimits the beginning and end of
 # each ad. Return the list of strings.
 # Hint: To build this reg-ex, you can use your text editor's regex search functionality.
-def undefined( TODO ):
-    '''TODO'''
-    TODO
-
+def page_to_ads(page):
+    '''Split "page" to a list of advertisement blocks.'''
+    rx = re.compile(r'<div class="ad">(.*?)<div class="clear">',
+                    re.DOTALL)
+    ads = re.findall(rx, page)
+    return ads
 
 # Define a function that takes a string corresponding to the block of one
 # advertisement and extracts from it the following data: Name, price, and
 # the description as displayed on the page.
-def undefined( TODO ):
+def get_dict_from_ad_block(block):
     '''TODO'''
-    TODO
+    rx = re.compile(r'.*?title="(?P<name>.*?)"'
+                    r'.*?redirectTo=(?P<url>.*?\.html)">',
+                    re.DOTALL)
+    data = re.search(rx, block)
+    dict = data.groupdict()
+    return dict
 
 
 
