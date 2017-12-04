8aa1a0dc9bf6460e7ac52eb25c974c677cd296be
Philipp G. Haselwarter <philipp@haselwarter.org>
2017-12-04 16:11:10 +0100

    Add instructions for mock midterm

---
+++ izpiti/2017-12-04/mock_exam-2017-12-04.ml
@@ -0,0 +1,54 @@
+(* ================= *)
+(* Part 1: functions *)
+(* ================= *)
+
+(* Write a function that takes two numbers and returns their sum. *)
+let add x y = ()
+
+(* Write a function f that adds 3 to its argument. *)
+let add3 = ()
+
+(* Write a function that adds 5 to all elements of a list. *)
+(* let add5_list ... *)
+
+(* Write a function that takes a triple of values and returns the third, e.g.
+   given (1, "horse", [None]), return [None]. *)
+
+
+(* Write a function that takes two functions g, f, and returns their composite
+   g o f*)
+
+
+
+(* ============================== *)
+(* Part 2: data types & recursion *)
+(* ============================== *)
+
+(* Rose trees are a tree data structure with variable number of branches per
+   node. We can represent them as a parametric type 'a rosetree with a single
+   constructor. This constructor takes a value of type 'a (the "root") and a
+   list of 'a rosetrees (the "forest") as arguments. *)
+type 'a rosetree = TODO
+
+(* Define a projection function that returns the root of a rose tree. *)
+let root t = ()
+
+(* Write a function that tests if a rose tree of integers contains a negative
+   integer. *)
+let rec neg_p t = ()
+
+
+
+(* Write a function wide_tree n that generates trees with a forest containing
+   n trees.
+   Hint: write a helper function that generates lists of a given length. *)
+
+
+
+(* Write a function that computes the size of a tree, i.e. the number of
+   nodes.
+   For full credit, your function should be tail recursive.
+
+   Remark: Like we observed in the tutorials on the example of List.map,
+   functions from the List module are not always tail recursive, so better
+   avoid them if you want your function to be tail recursive. *)
