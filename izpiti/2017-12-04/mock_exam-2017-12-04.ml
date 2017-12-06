ab3b5c294ab9e711966cb6f4b2f70b737199be57
Matija Pretnar <matija@pretnar.info>
2017-12-06 18:20:40 +0100

    Prevedel poskusni izpit

--- izpiti/2017-12-04/mock_exam-2017-12-04.ml
+++ izpiti/2017-12-04/mock_exam-2017-12-04.ml
@@ -1,54 +1,54 @@
-(* ================= *)
-(* Part 1: functions *)
-(* ================= *)
+(* =================== *)
+(* 1. naloga: funkcije *)
+(* =================== *)
 
-(* Write a function that takes two numbers and returns their sum. *)
-let add x y = ()
+(* 1.1) Definirajte funkcijo, ki vzame dve celi števili ter vrne njuno vsoto.
+   Primer: /sestej 2 3 = 5/ *)
+let sestej = failwith "dopolni me"
 
-(* Write a function f that adds 3 to its argument. *)
-let add3 = ()
+(* 1.2) Definirajte funkcijo, ki svojemu argumentu prišteje 3.
+   Primer: /pristej_tri 10 = 13/ *)
+let pristej_tri = failwith "dopolni me"
 
-(* Write a function that adds 5 to all elements of a list. *)
-(* let add5_list ... *)
+(* 1.3) Definirajte funkcijo, ki vsem elementom seznama prišteje 5.
+   Primer: /vsem_pristej_pet [1; 2] = [6; 7]/ *)
+let vsem_pristej_pet = failwith "dopolni me"
 
-(* Write a function that takes a triple of values and returns the third, e.g.
-   given (1, "horse", [None]), return [None]. *)
+(* 1.4) Definirajte funkcijo, ki vrne zadnjo komponento nabora s tremi elementi.
+   Primer: /tretji (1, "horse", [None]) = [None]/ *)
+let tretji = failwith "dopolni me"
 
-
-(* Write a function that takes two functions g, f, and returns their composite
-   g o f*)
-
-
-
-(* ============================== *)
-(* Part 2: data types & recursion *)
-(* ============================== *)
-
-(* Rose trees are a tree data structure with variable number of branches per
-   node. We can represent them as a parametric type 'a rosetree with a single
-   constructor. This constructor takes a value of type 'a (the "root") and a
-   list of 'a rosetrees (the "forest") as arguments. *)
-type 'a rosetree = TODO
-
-(* Define a projection function that returns the root of a rose tree. *)
-let root t = ()
-
-(* Write a function that tests if a rose tree of integers contains a negative
-   integer. *)
-let rec neg_p t = ()
+(* 1.5) Definirajte funkcijo, ki vzame dve funkciji ter vrne njun kompozitum.
+   Primer: /kompozitum succ string_of_int 5 = "6"/ *)
+let kompozitum = failwith "dopolni me"
 
 
+(* ======================================= *)
+(* 2. naloga: podatkovni tipi in rekurzija *)
+(* ======================================= *)
 
-(* Write a function wide_tree n that generates trees with a forest containing
-   n trees. You can choose an arbitrary value for the root of the k-th tree.
-   Hint: write a helper function that generates lists of a given length. *)
+(* 2.1) Rožno drevo je drevo, v katerem ima vsak koren poljubno mnogo otrok,
+   ki so zopet rožna drevesa. Rožna drevesa predstavimo s parametričnim
+   tipom /'a drevo/ z enim konstruktorjem, ki sprejme:
+   - vrednost (koren) tipa /'a/ in
+   - seznam (gozd) dreves tipa /'a drevo/. *)
+type 'a drevo = DopolniMe
 
+(* 2.2) Napišite funkcijo, ki vrne koren danega rožnega drevesa. *)
+let koren = failwith "dopolni me"
 
+(* 2.3) Napišite funkcijo, ki preveri, ali drevo celih števil vsebuje kakšno negativno število. *)
+let kaksno_negativno = failwith "dopolni me"
 
-(* Write a function that computes the size of a tree, i.e. the number of
-   nodes.
-   For full credit, your function should be tail recursive.
+(* 2.4) Sestavite funkcijo, ki sprejme naravno število ter sestavi (poljubno)
+   drevo, ki ima toliko otrok.
+   Namig: napišite pomožno funkcijo, ki ustvari poljuben seznam dane dolžine. *)
+let drevo_z_veliko_otroci = failwith "dopolni me"
 
-   Remark: Like we observed in the tutorials on the example of List.map,
-   functions from the List module are not always tail recursive, so better
-   avoid them if you want your function to be tail recursive. *)
+(* 2.5) Sestavite funkcijo, ki izračuna število vseh vozlišč v drevesu.
+   Če želite vse točke, mora biti funkcija repno rekurzivna.
+
+   Opomba: kot ste videli na vajah, nekatere funkcije iz modula List,
+   na primer List.map, niso repno rekurzivne, zato se jim raje
+   izognite. *)
+let velikost = failwith "dopolni me"
