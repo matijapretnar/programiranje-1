102ccd1161e8bc332a467bee8f7ba0eac03a18c0
Matija Pretnar <matija@pretnar.info>
2017-12-07 08:24:23 +0100

    Dodal poskusni izpit

---
+++ izpiti/2017-12-07/poskusni-izpit-2017-12-07.ml
@@ -0,0 +1,54 @@
+(* =================== *)
+(* 1. naloga: funkcije *)
+(* =================== *)
+
+(* 1.1) Definirajte funkcijo, ki vzame dve celi števili ter vrne njuno vsoto.
+   Primer: /sestej 2 3 = 5/ *)
+let sestej = failwith "dopolni me"
+
+(* 1.2) Definirajte funkcijo, ki svojemu argumentu prišteje 3.
+   Primer: /pristej_tri 10 = 13/ *)
+let pristej_tri = failwith "dopolni me"
+
+(* 1.3) Definirajte funkcijo, ki vsem elementom seznama prišteje 5.
+   Primer: /vsem_pristej_pet [1; 2] = [6; 7]/ *)
+let vsem_pristej_pet = failwith "dopolni me"
+
+(* 1.4) Definirajte funkcijo, ki vrne zadnjo komponento nabora s tremi elementi.
+   Primer: /tretji (1, "horse", [None]) = [None]/ *)
+let tretji = failwith "dopolni me"
+
+(* 1.5) Definirajte funkcijo, ki vzame dve funkciji ter vrne njun kompozitum.
+   Primer: /kompozitum succ string_of_int 5 = "6"/ *)
+let kompozitum = failwith "dopolni me"
+
+
+(* ======================================= *)
+(* 2. naloga: podatkovni tipi in rekurzija *)
+(* ======================================= *)
+
+(* 2.1) Rožno drevo je drevo, v katerem ima vsak koren poljubno mnogo otrok,
+   ki so zopet rožna drevesa. Rožna drevesa predstavimo s parametričnim
+   tipom /'a drevo/ z enim konstruktorjem, ki sprejme:
+   - vrednost (koren) tipa /'a/ in
+   - seznam (gozd) dreves tipa /'a drevo/. *)
+type 'a drevo = DopolniMe
+
+(* 2.2) Napišite funkcijo, ki vrne koren danega rožnega drevesa. *)
+let koren = failwith "dopolni me"
+
+(* 2.3) Napišite funkcijo, ki preveri, ali drevo celih števil vsebuje kakšno negativno število. *)
+let kaksno_negativno = failwith "dopolni me"
+
+(* 2.4) Sestavite funkcijo, ki sprejme naravno število ter sestavi (poljubno)
+   drevo, ki ima toliko otrok.
+   Namig: napišite pomožno funkcijo, ki ustvari poljuben seznam dane dolžine. *)
+let drevo_z_veliko_otroci = failwith "dopolni me"
+
+(* 2.5) Sestavite funkcijo, ki izračuna število vseh vozlišč v drevesu.
+   Če želite vse točke, mora biti funkcija repno rekurzivna.
+
+   Opomba: kot ste videli na vajah, nekatere funkcije iz modula List,
+   na primer List.map, niso repno rekurzivne, zato se jim raje
+   izognite. *)
+let velikost = failwith "dopolni me"
