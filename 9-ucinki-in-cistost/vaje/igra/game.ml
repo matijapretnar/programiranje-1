b7322b774014cb9d9cd11860b2a30d2476060286
Ziga <ziga.luksic@live.com>
2017-12-26 18:59:45 +0100

    Add solutins to exercise 9.

--- 9-ucinki-in-cistost/vaje/igra/game.ml
+++ 9-ucinki-in-cistost/vaje/igra/game.ml
@@ -2,54 +2,49 @@
 type result = Victory of Field.symbol | Draw
 
 (* Funkcija za zamenjavo igralcov. *)
-let switch_symbol = ()
-  (*/// Napiši funkcijo, ki pretvori Cricle v Cross, Cross v Circle in
-        vrednost Empty pusti nespremenjeno. Pri tem ne pozabi, da so ti
-        tipi definirani v modulu Field. ///*)
+let switch_symbol = function
+  | Field.Circle -> Field.Cross
+  | Field.Cross -> Field.Circle
+  | Field.Empty -> Field.Empty
 
 (* Funkcija, ki zahteva vhod igralca in poskusi narediti potezo. Pri tem
    vse neveljavne poteze prestreže. *)
 let rec make_move player field =
-  (*/// Izpiši kdo je na potezi. Vhod "player" je tipa Field.symbol. ///*)
+  print_endline ("It is the turn of player "^(Field.symbol_to_string player));
   try
-    print_string "Postavi v vrsto : ";
+    print_string "Place in row : ";
     let x = read_int () in
-    print_string "Postavi v stolpec : ";
+    print_string "Place in column : ";
     let y = read_int () in
-    (*/// Nastavi simbol na koordinatah x in y v polju field na znak igralca
-          player. ///*)
+    Field.set_symbol x y player field
   with
   (* Prestrezi in obravnavaj napake. *)
   (*/// Obravnavaj napake tako, da izpišeš sporočilo uporabniku in ponoviš
         potezo. Potezo ponoviš tako, da ponovno kličeš "make_move". ///*)
-  | Field.Coordinate_out_of_bounds ->
-    failwith "To Do"
-  | Field.Nonempty_value_at_coordinates ->
-    failwith "To Do"
-  | Failure msg when msg = "int_of_string" ->
-    failwith "To Do"
+        | Field.Coordinate_out_of_bounds ->
+          print_endline "Napačne koordinate. Poskusi ponovno."; make_move player field
+        | Field.Nonempty_value_at_coordinates ->
+          print_endline "Polje je že polno. Poskusi ponovno."; make_move player field
+        | Failure msg when msg = "int_of_string" ->
+          print_endline "Vnos mora biti celo število. Poskusi ponovno."; make_move player field
 
 (* Glavna zanka igre, ki izvaja poteze igralcev dokler se igra ne konča.
    Hkrati vsako potezo izpiše trenutno stanje. *)
-let rec game_loop player field = ()
-  (*/// Vhod "player" predstavlja igralca, ki je na potezi in "field" trenutno
-        stanje igralnega polja.
-        Najprej preveri, če je kdo že zmagal in v tem primeru vrni pravilen
-        rezultat (s pomočjo tipa, ki smo ga definirali na začetku datoteke).
-        Nato preveri ali je sploh še kakšna možna poteza. Če ni, vrni
-        rezultat, ki predstavlja neodločen izid.
-        Sicer pa uporabi spodnjo zakomentiratno kodo, da se izvede naslednja
-        poteza. ///*)
-
-  (*
-      (* Make a move. *)
+let rec game_loop player field =
+  (* Preveri, če je igre konec. *)
+  match Field.victory field with
+  | Some s -> Victory s (* Nekdo je zmagal. *)
+  | None ->
+    if Field.no_more_moves field then
+      Draw (* Nihče ne more zmagati. *)
+    else
+      (* Naredi poteo. *)
       let new_field = make_move player field in
-      (* Print field. *)
+      (* Izpiši polje. *)
       print_newline (); Field.print_field new_field; print_newline ();
-      (* Switch player and repeat loop. *)
+      (* Zamenjaj igralca in naredi potezo. *)
       let new_player = switch_symbol player in
       game_loop new_player new_field
-  *)
 
 (* Zažene igro na praznem polju. Po končani igri izpiše izzi in zažene novo
    igro. *)
