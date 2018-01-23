d44e11f070724e1c37b6041b09628c7327f80a23
Ziga <ziga.luksic@fmf.uni-lj.si>
2018-01-23 12:27:16 +0100

    Fix typo

--- izpiti/2018-01-24/filter_trees.ml
+++ izpiti/2018-01-24/filter_trees.ml
@@ -19,7 +19,7 @@
 type filter_tree = Node of int * filter_tree * filter_tree
                  | Box of int list
 
-let example_tee = Node(10,
+let example_tree = Node(10,
                 Node(5, (Box [1]), (Box [])),
                 Node(15, (Box []), (Box [19;20])))
 
