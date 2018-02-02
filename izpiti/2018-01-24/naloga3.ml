f30112ee5a44b3b7ff17f0319ec8b24e0d9510ab
Matija Pretnar <matija@pretnar.info>
2018-02-02 19:25:06 +0100

    Uskladil vzorčno datoteko z izpitom

--- izpiti/2018-01-24/naloga3.ml
+++ izpiti/2018-01-24/naloga3.ml
@@ -2,26 +2,17 @@ type vektor = int * int
 type matrika = int * int * int * int
 
 module type Linearna = sig
-  (* Osnovni tip modula. *)
+  (* Tip linearnih preslikav *)
   type t
-  (* Identiteta. *)
+  (* Identiteta *)
   val id : t
-  (* Izračun funkcije na podatkih. *)
-  val uporabi : vektor -> t -> vektor
-  (* Funkcija, ki sprejme matriko in jo pretvori v osnovni tip modula.
-     Če je osnovni tip modula matrika, pretvori matriko v matriko. *)
+  (* Dano preslikavo uporabi na vektorju *)
+  val uporabi : t -> vektor -> vektor
+  (* Vrne linearno preslikavo, določeno z matriko *)
   val iz_matrike : matrika -> t
-  (* Funkcija, ki sprejme funkcijo in jo pretvori v osnovni tip modula.
-     Če je osnovni tip modula matrika, pretvori funkcijo v matriko. *)
+  (* Vrne linearno preslikavo, določeno s funkcijo
+     (predpostavite lahko, da je funkcija linearna) *)
   val iz_funkcije : (vektor -> vektor) -> t
-  (* Vrne kompozitum dveh preslikav. *)
+  (* Vrne kompozitum danih preslikav. *)
   val kompozitum : t -> t -> t
-end
-
-module Matrika : Linearna = struct
-  (* Dopolni me! *)
-end
-
-module Funkcija : Linearna = struct
-  (* Dopolni me! *)
 end
