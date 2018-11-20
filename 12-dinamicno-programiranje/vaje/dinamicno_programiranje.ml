1f6e6c5128bc9d17410e94f1f8c864a8548771f8
Ziga <ziga.luksic@fmf.uni-lj.si>
2018-11-20 15:16:48 +0100

    Update exercises for dynamic programing 1

--- 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
+++ 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
@@ -1,40 +1,38 @@
+(* ========== Vaje 6: Dinamično programiranje  ========== *)
 
-(* ===== Vaja: Dinamično programiranje  ===== *)
 
-(* Požrešna miška se nahaja v zgornjem levem kotu šahovnice. Premikati se sme
-   samo za eno polje navzdol ali za eno polje na desno in na koncu prispeti v
-   desni spodnji kot. Na vsakem polju šahovnice je en sirček. Ti sirčki imajo
-   različne (pozitivne) mase. Miška bi se rada kar se da nažrla, zato jo zanima,
-   katero pot naj ubere.
+(*----------------------------------------------------------------------------*]
+ Požrešna miška se nahaja v zgornjem levem kotu šahovnice. Premikati se sme
+ samo za eno polje navzdol ali za eno polje na desno in na koncu mora prispeti
+ v desni spodnji kot. Na vsakem polju šahovnice je en sirček. Ti sirčki imajo
+ različne (ne-negativne) mase. Miška bi se rada kar se da nažrla, zato jo
+ zanima, katero pot naj ubere.
 
-   Napišite funkcijo "max_cheese cheese_matrix", ki dobi matriko z
-   masami sirčkov in vrne največjo skupno maso, ki jo bo miška požrla, če gre po
-   optimalni poti.
+ Funkcija [max_cheese cheese_matrix], ki dobi matriko [cheese_matrix] z masami
+ sirčkov in vrne največjo skupno maso, ki jo bo miška požrla, če gre po
+ optimalni poti.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # max_cheese test_matrix;;
+ - : int = 13
+[*----------------------------------------------------------------------------*)
 
-   ----------
-   # max_cheese cheese_matrix;;
-   - : int = 13
-   ----------*)
+let test_matrix = 
+  [| [| 1 ; 2 ; 0 |];
+     [| 2 ; 4 ; 5 |];
+     [| 7 ; 0 ; 1 |] |]
 
-let test_matrix = [| [| 1 ; 2 ; 0 |];
-                     [| 2 ; 4 ; 5 |];
-                     [| 7 ; 0 ; 1 |]  |]
+(*----------------------------------------------------------------------------*]
+ Rešujemo problem sestavljanja alternirajoče obarvanih stolpov. Imamo štiri
+ različne tipe gradnikov, dva modra in dva rdeča. Modri gradniki so višin 2 in
+ 3, rdeči pa višin 1 in 2.
 
-let max_cheese cheese_matrix = ()
+ Funkcija [alternating_towers] za podano višino vrne število različnih stolpov
+ dane višine, ki jih lahko zgradimo z našimi gradniki, kjer se barva gradnikov
+ v stolpu izmenjuje (rdeč na modrem, moder na rdečem itd.). Začnemo z gradnikom
+ poljubne barve.
 
-(* Rešujemo problem stolpov, ko smo ga spoznali na predavanjih.
-   Imamo štiri različne tipe gradnikov, dva modra in dva rdeča.
-   Modri gradniki so višin 2 in 3, rdeči pa višin 1 in 2.
-
-   Napiši funkcijo "alternating_towers height", ki za podano višino "height"
-   izpiše število različnih stolpov podane višine, kjer se barva gradnikov
-   izmenjuje (rdeč na modrem, moder na rdečem itd.).
-
-   Namig: Uporabi dve pomožni funkciji. Za medsebojno rekurzijo uporabi
-          ukaz "and".
-   ----------
-   # alternating_towers 10;;
-   - : int = 35
-   ---------- *)
-
-let alternating_towers height = ()
+ Namig: Uporabi medsebojno rekurzivni pomožni funkciji z ukazom [and].
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # alternating_towers 10;;
+ - : int = 35
+[*----------------------------------------------------------------------------*)
