db2cea16bd4fe4e94b2547e9ffbda63f44953e1f
Ziga <ziga.luksic@fmf.uni-lj.si>
2018-11-20 15:14:55 +0100

    Update exercises for dynamic programing 1

--- 12-dinamicno-programiranje/exercises/dynamic_programing.ml
+++ 12-dinamicno-programiranje/exercises/dynamic_programing.ml
@@ -1,16 +1,18 @@
-(* ========== Vaje 6: Dinamično programiranje  ========== *)
+
+
+(* ========== Exercises 6: Dynamic programing  ========== *)
 
 
 (*----------------------------------------------------------------------------*]
- Požrešna miška se nahaja v zgornjem levem kotu šahovnice. Premikati se sme
- samo za eno polje navzdol ali za eno polje na desno in na koncu mora prispeti
- v desni spodnji kot. Na vsakem polju šahovnice je en sirček. Ti sirčki imajo
- različne (ne-negativne) mase. Miška bi se rada kar se da nažrla, zato jo
- zanima, katero pot naj ubere.
+ The gluttonous mouse is located in the left upper corner of a matrix. It can
+ only move one field down or one field right and at the end it must arrive at
+ the lower right corner. On every square of the field there is a given
+ (non-negative) amount of cheese. The mouse wants to eat as much as possible
+ and its trying to figure out the optimal way.
 
- Funkcija [max_cheese cheese_matrix], ki dobi matriko [cheese_matrix] z masami
- sirčkov in vrne največjo skupno maso, ki jo bo miška požrla, če gre po
- optimalni poti.
+ Write the function [max_cheese cheese_matrix] that given a matrix of cheese
+ amounts calculates the overall amount of cheese that the mouse will eat if it
+ follows the optimal way.
  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  # max_cheese test_matrix;;
  - : int = 13
@@ -33,16 +35,15 @@ let max_cheese cheese_matrix =
   best_path 0 0
 
 (*----------------------------------------------------------------------------*]
- Rešujemo problem sestavljanja alternirajoče obarvanih stolpov. Imamo štiri
- različne tipe gradnikov, dva modra in dva rdeča. Modri gradniki so višin 2 in
- 3, rdeči pa višin 1 in 2.
+ We are solving the problem of alternatingly colored towers. There are four
+ different types of building blocks, two of them blue and two red. The blue
+ blocks have heights 2 and 3 and the red ones 1 and 2.
 
- Funkcija [alternating_towers] za podano višino vrne število različnih stolpov
- dane višine, ki jih lahko zgradimo z našimi gradniki, kjer se barva gradnikov
- v stolpu izmenjuje (rdeč na modrem, moder na rdečem itd.). Začnemo z gradnikom
- poljubne barve.
+ Write the function [alternating_towers] for a given height calculates the
+ number of different towers of given height that we can build using alternatingly
+ colored blocks (red on blue, blue on red etc.). We may start with any color.
 
- Namig: Uporabi medsebojno rekurzivni pomožni funkciji z ukazom [and].
+ Hint: Use two mutually recursive auxilary functions using the keyword "and".
  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
  # alternating_towers 10;;
  - : int = 35
