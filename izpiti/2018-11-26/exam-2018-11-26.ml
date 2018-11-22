907cc052cdf14bdc56ccfbba454c1b4fc6f3dac2
Philipp G. Haselwarter <philipp@haselwarter.org>
2018-11-22 16:30:00 +0100

    Diversify names

--- izpiti/2018-11-26/exam-2018-11-26.ml
+++ izpiti/2018-11-26/exam-2018-11-26.ml
@@ -40,12 +40,12 @@ type status = Staff | Passenger of priority
 (* Given *)
 type flyer = { status : status ; name : string }
 
-let flyers = [ {status = Staff; name = "Irene"}
-             ; {status = Passenger (Group 0); name = "Christine"}
-             ; {status = Passenger Top; name = "Jane"}
-             ; {status = Passenger (Group 1000); name = "Anja"}
-             ; {status = Passenger (Group 1000); name = "Xavier"}
-             ; {status = Staff; name = "Matija"}
+let flyers = [ {status = Staff; name = "Quinn"}
+             ; {status = Passenger (Group 0); name = "Xiao"}
+             ; {status = Passenger Top; name = "Jaina"}
+             ; {status = Passenger (Group 1000); name = "Aleks"}
+             ; {status = Passenger (Group 1000); name = "Robin"}
+             ; {status = Staff; name = "Alan"}
              ]
 
 (* 2 pts *)
