b4b20e93f1bd81a9ac110d3b051afe1838244e29
Matija Pretnar <matija@pretnar.info>
2019-09-25 15:13:08 +0200

    Pobrisal stare datoteke

--- stara-gradiva/hs-3-definicije-tipov/predavanja/Barve.hs
+++
@@ -1,5 +0,0 @@
-type Barva = String
-type Kocka = (Int, Int, Barva)
-
-posvetli :: Barva -> Barva
-posvetli b = "svetlo " ++ b
--- stara-gradiva/hs-4-razredi-tipov/predavanja/Foldable.hs
+++
@@ -1,9 +0,0 @@
-data Drevo a
-  = P
-  | S (Drevo a) a (Drevo a)
-  deriving Show
-
-instance  Foldable Drevo  where
-    foldr f z P = z
-    foldr f z (S l x d) =
-      foldr f (f x (foldr f z d)) l
