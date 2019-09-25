9876c23270a70a4033b895e124193a0d5e67b00e
Matija Pretnar <matija@pretnar.info>
2019-09-25 15:11:33 +0200

    Pobrisal angleške datoteke

--- 13-memoizacija/vaje/fib.py
+++ 13-memoizacija/vaje/fib.py
@@ -1,34 +1,33 @@
 from functools import lru_cache
 
-# Goal: compute the values of the Fibonacci sequence at 100, 500, 1000, 10**5, and 10**6
-# For each of the definitions, try how big a value you can compute. Why does it
-# fail there?
+# Cilj: izračunajte vrednosti Fibonaccijevega zaporadja za 100, 500, 1000,
+# 10**5, and 10**6 člen.
+# Za vsako definicijo preizkusite kako pozne člene lahko izračuante in poglejte
+# zakaj se pojavi problem (neučinkovitost, pregloboka rekurzija,
+# premalo spomina ...).
 
-# First define fib as a recursive function.
+# Definirajte naivno rekurzivno različico.
+# Omejitev: Prepočasno.
 def fib(n):
 
-# Define fib as a recursive function, but using the lru_cache decorater to
-# memoize results.
+# Z uporabo dekoratorja izboljšajte naivno različico.
+# Omejitev: Preseže največjo dovoljeno globino rekurzija za ~350.
 @...
 def fib_cache(n):
 
-# Draw the call tree for n = 5 and identify which subproblems are repeated.
+# Nariši drevo klicov za navadno rekurzivno fib funkcijo pri n=5 in
+# ugotovi kateri podproblemi so klicani večkrat.
 
-#   MAKE A DRAWING
-
-
-# Define fib recursively and manually memoize results.
+# Definirajte rekurzivno memoizirano funkcijo fib brez uporabe dekoratorja.
+# Omejitev: Preseže največjo dovoljeno globino rekurzija za ~1000.
 def fib_memo_rec(n):
 
-# Make a new drawing where you merge the repeated nodes in the tree. Which
-# subproblems does each call depend on directly?
+# Na katere podprobleme se direktno skicuje rekurzivna definicija fib?
 
-#   I DON'T SEE YOU DRAWING
-
-
-# Define fib as a dynamic program that fills up the table of results from the bottom.
+# Definirajte fib ki gradi rezultat od spodaj navzgor (torej računa in si zapomni
+# vrednosti od 1 proti n.)
 def fib_memo_iter(n):
 
-# Define fib as a dynamic program that only keeps those intermediate results
-# around that are needed to compute the next step.
+# Izboljšajte prejšnjo različico tako, da hrani zgolj rezultate, ki jih v
+# nadaljevanju nujno potrebuje.
 def fib_iter(n):
