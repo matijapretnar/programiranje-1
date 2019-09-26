8bf58f90d1c64899934a6624c31cf330502f4344
Matija Pretnar <matija@pretnar.info>
2019-09-26 15:55:08 +0200

    Datoteka za ustni izpit

---
+++ izpiti/ustni-izpit.py
@@ -0,0 +1,19 @@
+ocaml = [
+    '05-funkcijsko-programiranje',
+    '06-definicije-tipov',
+    '07-ucinki-in-cistost',
+    '09-modularnost',
+]
+psa = [
+    '08-iskalna-drevesa',
+    '10-spremenljive-podatkovne-strukture',
+    '11-deli-in-vladaj',
+    '12-dinamicno-programiranje',
+    '13-memoizacija',
+]
+import random
+
+input('Si pripravljen na vprašanje iz funkcijskega programiranja?')
+print(random.choice(ocaml))
+input('Si pripravljen na vprašanje iz podatkovnih struktur in algoritmov?')
+print(random.choice(psa))
