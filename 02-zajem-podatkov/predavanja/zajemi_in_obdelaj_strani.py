5cd5e33d416af124793ed94c1681d27fe9631d43
Matija Pretnar <matija@pretnar.info>
2019-09-26 15:48:18 +0200

    Uredil datoteke z zajetimi in obdelanimi podatki

--- 02-zajem-podatkov/predavanja/zajemi_in_obdelaj_strani.py
+++ 02-zajem-podatkov/predavanja/zajemi_in_obdelaj_strani.py
@@ -1,95 +1,160 @@
-import csv
-import json
-import os
 import re
-import sys
+import orodja
 
-import requests
+vzorec_bloka = re.compile(
+    r'<div class="lister-item mode-advanced">.*?'
+    r'</p>\s*</div>\s*</div>',
+    flags=re.DOTALL
+)
 
-
-def pripravi_imenik(ime_datoteke):
-    '''Če še ne obstaja, pripravi prazen imenik za dano datoteko.'''
-    imenik = os.path.dirname(ime_datoteke)
-    if imenik:
-        os.makedirs(imenik, exist_ok=True)
-
-
-def shrani_spletno_stran(url, ime_datoteke, vsili_prenos=False):
-    '''Vsebino strani na danem naslovu shrani v datoteko z danim imenom.'''
-    try:
-        print('Shranjujem {} ...'.format(url), end='')
-        sys.stdout.flush()
-        if os.path.isfile(ime_datoteke) and not vsili_prenos:
-            print('shranjeno že od prej!')
-            return
-        r = requests.get(url)
-    except requests.exceptions.ConnectionError:
-        print('stran ne obstaja!')
-    else:
-        pripravi_imenik(ime_datoteke)
-        with open(ime_datoteke, 'w', encoding='utf-8') as datoteka:
-            datoteka.write(r.text)
-            print('shranjeno!')
-
-
-def vsebina_datoteke(ime_datoteke):
-    '''Vrne niz z vsebino datoteke z danim imenom.'''
-    with open(ime_datoteke, encoding='utf-8') as datoteka:
-        return datoteka.read()
-
-
-def zapisi_csv(slovarji, imena_polj, ime_datoteke):
-    '''Iz seznama slovarjev ustvari CSV datoteko z glavo.'''
-    pripravi_imenik(ime_datoteke)
-    with open(ime_datoteke, 'w', encoding='utf-8') as csv_datoteka:
-        writer = csv.DictWriter(csv_datoteka, fieldnames=imena_polj)
-        writer.writeheader()
-        for slovar in slovarji:
-            writer.writerow(slovar)
-
-
-def zapisi_json(objekt, ime_datoteke):
-    '''Iz danega objekta ustvari JSON datoteko.'''
-    pripravi_imenik(ime_datoteke)
-    with open(ime_datoteke, 'w', encoding='utf-8') as json_datoteka:
-        json.dump(objekt, json_datoteka, indent=4, ensure_ascii=False)
-
-
-vzorec = re.compile(
-    r'href="/title/tt(?P<id>\d+)/\?ref_=adv_li_tt"[^>]*?'
-    r'>(?P<naslov>.+?)</a>.*?'
+vzorec_filma = re.compile(
+    r'<a href="/title/tt(?P<id>\d+)/".*?'
+    r'img alt="(?P<naslov>.+?)".*?'
     r'lister-item-year text-muted unbold">.*?\((?P<leto>\d{4})\)</span>.*?'
     r'runtime">(?P<dolzina>\d+?) min</.*?'
+    r'<span class="genre">(?P<zanri>.*?)</span>.*?'
     r'<strong>(?P<ocena>.+?)</strong>.*?'
-    r'<p class="text-muted">(?P<opis>.+?)<.*?',
-    re.DOTALL
+    r'<p class="text-muted">(?P<opis>.+?)</p.*?'
+    r'Directors?:(?P<reziserji>.+?)(<span class="ghost">|</p>).*?'
+    r'Votes:.*?data-value="(?P<glasovi>\d+)"',
+    flags=re.DOTALL
+)
+
+vzorec_osebe = re.compile(
+    r'<a\s+href="/name/nm(?P<id>\d+)/?[^>]*?>(?P<ime>.+?)</a>',
+    flags=re.DOTALL
+)
+
+vzorec_zasluzka = re.compile(
+    r'Gross:.*?data-value="(?P<zasluzek>(\d|,)+)"',
+    flags=re.DOTALL
+)
+
+vzorec_metascore = re.compile(
+    r'<span class="metascore.*?">(?P<metascore>\d+)',
+    flags=re.DOTALL
+)
+
+vzorec_oznake = re.compile(
+    r'<span class="certificate">(?P<oznaka>.+?)</span>',
+    flags=re.DOTALL
+)
+
+vzorec_daljsi_povzetek = re.compile(
+    r'<a href="/title/tt\d+/plotsummary.*?&nbsp;&raquo;',
+    flags=re.DOTALL
+)
+
+vzorec_igralcev = re.compile(
+    r'Stars?:(?P<igralci>.+?)</p>.*?',
+    flags=re.DOTALL
 )
 
 
-def izloci_podatke_filma(ujemanje_filma):
-    podatki_filma = ujemanje_filma.groupdict()
-    podatki_filma['opis'] = podatki_filma['opis'].strip()
-    podatki_filma['leto'] = int(podatki_filma['leto'])
-    podatki_filma['dolzina'] = int(podatki_filma['dolzina'])
-    podatki_filma['ocena'] = float(podatki_filma['ocena'].replace(',', '.'))
-    return podatki_filma
+def izloci_osebe(niz):
+    osebe = []
+    for oseba in vzorec_osebe.finditer(niz):
+        osebe.append({
+            'id': int(oseba.groupdict()['id']),
+            'ime': oseba.groupdict()['ime'],
+        })
+    return osebe
 
 
-for i in range(1, 11):
+def izloci_podatke_filma(blok):
+    film = vzorec_filma.search(blok).groupdict()
+    film['id'] = int(film['id'])
+    film['dolzina'] = int(film['dolzina'])
+    film['zanri'] = film['zanri'].strip().split(', ')
+    film['leto'] = int(film['leto'])
+    # odstranimo morebitno povezavo na daljši posnetek
+    film['opis'] = vzorec_daljsi_povzetek.sub('', film['opis'])
+    # odstranimo morebitne povezave na osebe
+    film['opis'] = vzorec_osebe.sub(r'\2', film['opis'])
+    film['opis'] = film['opis'].strip()
+    film['ocena'] = float(film['ocena'])
+    film['glasovi'] = int(film['glasovi'])
+    film['reziserji'] = izloci_osebe(film['reziserji'])
+    # zabeležimo oznako, če je omenjena
+    oznaka = vzorec_oznake.search(blok)
+    if oznaka:
+        film['oznaka'] = oznaka['oznaka']
+    else:
+        film['oznaka'] = None
+    # zabeležimo igralce, če so omenjeni
+    igralci = vzorec_igralcev.search(blok)
+    if igralci:
+        film['igralci'] = izloci_osebe(igralci['igralci'])
+    else:
+        film['igralci'] = []
+    # zabeležimo zaslužek, če je omenjen
+    zasluzek = vzorec_zasluzka.search(blok)
+    if zasluzek:
+        film['zasluzek'] = int(zasluzek['zasluzek'].replace(',', ''))
+    else:
+        film['zasluzek'] = None
+    # zabeležimo metascore, če je omenjen
+    metascore = vzorec_metascore.search(blok)
+    if metascore:
+        film['metascore'] = int(metascore['metascore'])
+    else:
+        film['metascore'] = None
+    return film
+
+
+def filmi_na_strani(st_strani, na_stran=250):
     url = (
-        'https://www.imdb.com/search/title'
-        '?sort=num_votes,desc&title_type=feature&count=250'
-        '&page={}'
-    ).format(i)
-    shrani_spletno_stran(url, 'zajeti-podatki/najbolj-znani-filmi-{}.html'.format(i))
+        f'https://www.imdb.com/search/title'
+        f'?sort=num_votes,desc&title_type=feature&count={na_stran}'
+        f'&start={(st_strani - 1) * na_stran + 1}'
+    )
+    ime_datoteke = 'zajeti-podatki/najbolj-znani-filmi-{}.html'.format(
+        st_strani)
+    orodja.shrani_spletno_stran(url, ime_datoteke)
+    vsebina = orodja.vsebina_datoteke(ime_datoteke)
+    for blok in vzorec_bloka.finditer(vsebina):
+        yield izloci_podatke_filma(blok.group(0))
 
 
-podatki_filmov = []
-for i in range(1, 11):
-    vsebina = vsebina_datoteke(
-        'zajeti-podatki/najbolj-znani-filmi-{}.html'.format(i))
-    for ujemanje_filma in vzorec.finditer(vsebina):
-        podatki_filmov.append(izloci_podatke_filma(ujemanje_filma))
-zapisi_json(podatki_filmov, 'obdelani-podatki/vsi-filmi.json')
-zapisi_csv(podatki_filmov, ['id', 'naslov', 'dolzina', 'leto',
-                            'ocena', 'opis'], 'obdelani-podatki/vsi-filmi.csv')
+def izloci_gnezdene_podatke(filmi):
+    osebe, vloge, zanri = [], [], []
+    videne_osebe = set()
+
+    for film in filmi:
+        for zanr in film.pop('zanri'):
+            zanri.append({'film': film['id'], 'zanr': zanr})
+        for reziser in film.pop('reziserji'):
+            if reziser['id'] not in videne_osebe:
+                videne_osebe.add(reziser['id'])
+                osebe.append(reziser)
+            vloge.append(
+                {'film': film['id'], 'oseba': reziser['id'], 'vloga': 'reziser'})
+        for igralec in film.pop('igralci'):
+            if igralec['id'] not in videne_osebe:
+                videne_osebe.add(igralec['id'])
+                osebe.append(igralec)
+            vloge.append(
+                {'film': film['id'], 'oseba': igralec['id'], 'vloga': 'igralec'})
+
+    osebe.sort(key=lambda oseba: oseba['id'])
+    vloge.sort(key=lambda vloga: (
+        vloga['film'], vloga['oseba'], vloga['vloga']))
+    zanri.sort(key=lambda zanr: (zanr['film'], zanr['zanr']))
+
+    return osebe, vloge, zanri
+
+
+filmi = []
+for st_strani in range(1, 41):
+    for film in filmi_na_strani(st_strani, 250):
+        filmi.append(film)
+filmi.sort(key=lambda film: film['id'])
+orodja.zapisi_json(filmi, 'obdelani-podatki/filmi.json')
+osebe, vloge, zanri = izloci_gnezdene_podatke(filmi)
+orodja.zapisi_csv(
+    filmi,
+    ['id', 'naslov', 'dolzina', 'leto', 'ocena', 'metascore', 'glasovi', 'zasluzek', 'oznaka', 'opis'], 'obdelani-podatki/filmi.csv'
+)
+orodja.zapisi_csv(osebe, ['id', 'ime'], 'obdelani-podatki/osebe.csv')
+orodja.zapisi_csv(vloge, ['film', 'oseba', 'vloga'], 'obdelani-podatki/vloge.csv')
+orodja.zapisi_csv(zanri, ['film', 'zanr'], 'obdelani-podatki/zanri.csv')
