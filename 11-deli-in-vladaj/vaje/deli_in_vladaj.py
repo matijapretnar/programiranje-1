420af2492d16fccceda6876f4da683a46dbda84c
zigaLuksic <ziga.luksic@fmf.uni-lj.si>
2019-09-27 14:17:29 +0200

    Change order of exercises

--- 11-deli-in-vladaj/vaje/deli_in_vladaj.py
+++ 11-deli-in-vladaj/vaje/deli_in_vladaj.py
@@ -30,22 +30,6 @@
 
 
 ##############################################################################
-# Tabelo a želimo urediti z algoritmom hitrega urejanja (quicksort).
-#
-# Napišite funkcijo [quicksort(a)], ki uredi tabelo [a] s pomočjo pivotiranja.
-# Poskrbi, da algoritem deluje 'na mestu', torej ne uporablja novih tabel.
-#
-# Namig: Definirajte pomožno funkcijo [quicksort_part(a, start, end)], ki
-#        uredi zgolj del tabele [a].
-#
-#   >>> a = [10, 4, 5, 15, 11, 3, 17, 2, 18]
-#   >>> quicksort(a)
-#   [2, 3, 4, 5, 10, 11, 15, 17, 18]
-##############################################################################
-
-
-
-##############################################################################
 # V tabeli želimo poiskati vrednost k-tega elementa po velikosti.
 #
 # Primer: Če je
@@ -58,4 +42,20 @@
 # Sestavite funkcijo [kth_element(a, k)], ki v tabeli [a] poišče [k]-ti
 # element po velikosti. Funkcija sme spremeniti tabelo [a]. Cilj naloge je, da
 # jo rešite brez da v celoti uredite tabelo [a].
+##############################################################################
+
+
+
+##############################################################################
+# Tabelo a želimo urediti z algoritmom hitrega urejanja (quicksort).
+#
+# Napišite funkcijo [quicksort(a)], ki uredi tabelo [a] s pomočjo pivotiranja.
+# Poskrbi, da algoritem deluje 'na mestu', torej ne uporablja novih tabel.
+#
+# Namig: Definirajte pomožno funkcijo [quicksort_part(a, start, end)], ki
+#        uredi zgolj del tabele [a].
+#
+#   >>> a = [10, 4, 5, 15, 11, 3, 17, 2, 18]
+#   >>> quicksort(a)
+#   [2, 3, 4, 5, 10, 11, 15, 17, 18]
 ##############################################################################
