8e0a76d861570ea349df827aaf50278afd0d9ee8
Matija Pretnar <matija@pretnar.info>
2019-10-02 14:39:26 +0200

    Datoteke s predavanj o regularnih izrazih

---
+++ 01-regularni-izrazi/predavanja/preberi_filme.py
@@ -0,0 +1,23 @@
+import re
+
+with open('250-najbolj-znanih-filmov.html') as f:
+    vsebina = f.read()
+
+vzorec = (
+    r'<a href="/title/tt'
+    r'(?P<id>\d{7})'  # ID ima sedem števk
+    r'/\?ref_=adv_li_tt"\n>'  # neka šara vmes med id-jem in naslovom
+    r'(?P<naslov>.*?)'  # zajamemo naslov
+    r'</a>'
+    r'\s+'
+    r'<span class="lister-item-year text-muted unbold">\('
+    r'(?P<leto>\d{4})'
+    r'\)'
+)
+
+count = 0
+for zadetek in re.finditer(vzorec, vsebina):
+    print(zadetek.groupdict())
+    count += 1
+print(count)
+
