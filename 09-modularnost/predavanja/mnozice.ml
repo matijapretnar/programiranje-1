76a5b4d3b7b39b592c07c5ed9ea104735a72a7e8
Docker <docker@example.com>
2019-12-24 11:15:23 +0000

    Datoteke z moduli

--- 09-modularnost/predavanja/mnozice.ml
+++ 09-modularnost/predavanja/mnozice.ml
@@ -11,184 +11,169 @@ module MnozicaSeznami : Mnozica = struct
 
   let prazna = []
 
-  let vsebuje = List.mem
-  
-  let dodaj x mn = if vsebuje x mn then mn else x :: mn
+  let velikost m = List.length m
 
-  let velikost = List.length
+  let vsebuje x m = List.mem x m
+
+  let dodaj x m = if vsebuje x m then m else x :: m
 end
 
 module MnozicaDrevesa : Mnozica = struct
-    type 'a mn =
-        | Prazno
-        | Sestavljeno of 'a mn * 'a * 'a mn
+  type 'a drevo =
+    | Prazno
+    | Sestavljeno of 'a drevo * 'a * 'a drevo
+  
+  type 'a mn = 'a drevo
 
-    let rec vsebuje x = function
-    | Prazno -> false
-    | Sestavljeno (l, y, d) ->
-        if x < y then
-            vsebuje x l
-        else if y < x then
-            vsebuje x d
-        else
-            x = y
+  let prazna = Prazno
 
-    let rec dodaj x = function
-    | Prazno -> Sestavljeno (Prazno, x, Prazno)
-    | Sestavljeno (l, y, d) as drevo ->
-        if x < y then
-            Sestavljeno (dodaj x l, y, d)
-        else if y < x then
-            Sestavljeno (l, y, dodaj x d)
-        else
-            drevo
-
-    let rec velikost = function
+  let rec velikost = function
     | Prazno -> 0
     | Sestavljeno (l, _, d) -> 1 + velikost l + velikost d
 
-    let prazna = Prazno
+  let rec vsebuje x = function
+    | Prazno -> false
+    | Sestavljeno (l, y, d) when x < y -> vsebuje x l
+    | Sestavljeno (l, y, d) when x > y -> vsebuje x d
+    | drevo -> true
+
+  let rec dodaj x = function
+    | Prazno -> Sestavljeno (Prazno, x, Prazno)
+    | Sestavljeno (l, y, d) when x < y -> Sestavljeno (dodaj x l, y, d)
+    | Sestavljeno (l, y, d) when x > y -> Sestavljeno (l, y, dodaj x d)
+    | drevo -> drevo
 end
 
 module MnozicaNeumnaAVLDrevesa : Mnozica = struct
-    type 'a mn =
+    type 'a drevo =
         | Prazno
-        | Sestavljeno of 'a mn * 'a * 'a mn
+        | Sestavljeno of 'a drevo * 'a * 'a drevo
+
+    type 'a mn = 'a drevo
 
     let prazna = Prazno
 
-    let rec vsebuje x = function
-      | Prazno -> false
-      | Sestavljeno (l, y, d) ->
-          if x < y then
-            vsebuje x l
-          else if y < x then
-            vsebuje x d
-          else
-            x = y
-
     let rec velikost = function
       | Prazno -> 0
-      | Sestavljeno (l, _, d) ->
-            1 + velikost l + velikost d
+      | Sestavljeno (l, _, d) -> 1 + velikost l + velikost d
+
+    let rec vsebuje x = function
+      | Prazno -> false
+      | Sestavljeno (l, y, d) when x < y -> vsebuje x l
+      | Sestavljeno (l, y, d) when x > y -> vsebuje x d
+      | drevo -> true
 
     let zavrti_levo = function
-      | Sestavljeno (l, x, Sestavljeno (dl, y, dd)) ->
-          Sestavljeno (Sestavljeno (l, x, dl), y, dd)
-      | _ -> assert false
+        | Sestavljeno (l, x, Sestavljeno (dl, y, dd)) ->
+            Sestavljeno (Sestavljeno (l, x, dl), y, dd)
+        | _ -> failwith "Tega drevesa ne morem zavrteti"
 
     let zavrti_desno = function
-      | Sestavljeno (Sestavljeno (ll, y, dl), x, d) ->
-          Sestavljeno (ll, y, Sestavljeno (dl, x, d))
-      | _ -> assert false
+        | Sestavljeno (Sestavljeno (ll, y, dl), x, d) ->
+            Sestavljeno (ll, y, Sestavljeno (dl, x, d))
+        | _ -> failwith "Tega drevesa ne morem zavrteti"
 
     let rec visina = function
-      | Prazno -> 0
-      | Sestavljeno (l, _, d) -> 1 + max (visina l) (visina d)
+        | Prazno -> 0
+        | Sestavljeno (l, _, d) -> 1 + max (visina l) (visina d)
 
     let razlika = function
-      | Prazno -> 0
-      | Sestavljeno (l, _, d) -> visina l - visina d
+        | Prazno -> 0
+        | Sestavljeno (l, _, d) -> visina l - visina d
 
     let uravnotezi drevo =
-      match drevo with
-      | Prazno -> Prazno
-      | Sestavljeno (l, x, d)
-          when razlika drevo = 2 && razlika l = 1 ->
+        match drevo with
+        | Prazno -> Prazno
+        | Sestavljeno (l, x, d)
+            when razlika drevo = 2 && razlika l = 1 ->
             zavrti_desno drevo
-      | Sestavljeno (l, x, d)
-          when razlika drevo = 2  ->
+        | Sestavljeno (l, x, d)
+            when razlika drevo = 2  ->
             Sestavljeno (zavrti_levo l, x, d) |> zavrti_desno
-      | Sestavljeno (l, x, d)
-          when razlika drevo = -2 && razlika d = -1 ->
+        | Sestavljeno (l, x, d)
+            when razlika drevo = -2 && razlika d = -1 ->
             zavrti_levo drevo
-      | Sestavljeno (l, x, d)
-          when razlika drevo = -2 ->
+        | Sestavljeno (l, x, d)
+            when razlika drevo = -2 ->
             Sestavljeno (l, x, zavrti_desno d) |> zavrti_levo
-      | _ -> drevo
+        | _ -> drevo
 
     let rec dodaj x = function
       | Prazno -> Sestavljeno (Prazno, x, Prazno)
-      | Sestavljeno (l, y, d) as drevo ->
-          if x < y then
-            Sestavljeno (dodaj x l, y, d) |> uravnotezi
-          else if y < x then
-            Sestavljeno (l, y, dodaj x d) |> uravnotezi
-          else
-            drevo
+      | Sestavljeno (l, y, d) when x < y -> Sestavljeno (dodaj x l, y, d) |> uravnotezi
+      | Sestavljeno (l, y, d) when x > y -> Sestavljeno (l, y, dodaj x d) |> uravnotezi
+      | drevo -> drevo
+
 end
 
 
 module MnozicaAVLDrevesa : Mnozica = struct
-    type 'a mn =
+     type 'a drevo =
         | Prazno
-        | Sestavljeno of 'a mn * 'a * 'a mn * int
-    
-    let rec visina = function
-      | Prazno -> 0
-      | Sestavljeno (l, _, d, h) -> h
+        | Sestavljeno of 'a drevo * 'a * 'a drevo * int
 
-    let sestavljeno (l, x, d) =
-      let h = 1 + max (visina l) (visina d) in
-      Sestavljeno (l, x, d, h)
+    type 'a mn = 'a drevo
 
     let prazna = Prazno
 
-    let rec vsebuje x = function
-      | Prazno -> false
-      | Sestavljeno (l, y, d, _) ->
-          if x < y then
-            vsebuje x l
-          else if y < x then
-            vsebuje x d
-          else
-            x = y
-
     let rec velikost = function
       | Prazno -> 0
-      | Sestavljeno (l, _, d, _) ->
-            1 + velikost l + velikost d
+      | Sestavljeno (l, _, d, _) -> 1 + velikost l + velikost d
+
+    let rec vsebuje x = function
+      | Prazno -> false
+      | Sestavljeno (l, y, d, _) when x < y -> vsebuje x l
+      | Sestavljeno (l, y, d, _) when x > y -> vsebuje x d
+      | drevo -> true
+
+    let rec visina = function
+        | Prazno -> 0
+        | Sestavljeno (_, _, _, h) -> h
+
+    let sestavljeno (l, x, d) =
+        let h = 1 + max (visina l) (visina d) in
+        Sestavljeno (l, x, d, h)
 
     let zavrti_levo = function
-      | Sestavljeno (l, x, Sestavljeno (dl, y, dd, _), _) ->
-          sestavljeno (sestavljeno (l, x, dl), y, dd)
-      | _ -> assert false
+        | Sestavljeno (l, x, Sestavljeno (dl, y, dd, _), _) ->
+            sestavljeno (sestavljeno (l, x, dl), y, dd)
+        | _ -> failwith "Tega drevesa ne morem zavrteti"
 
     let zavrti_desno = function
-      | Sestavljeno (Sestavljeno (ll, y, dl, _), x, d, _) ->
-          sestavljeno (ll, y, sestavljeno (dl, x, d))
-      | _ -> assert false
+        | Sestavljeno (Sestavljeno (ll, y, dl, _), x, d, _) ->
+            sestavljeno (ll, y, sestavljeno (dl, x, d))
+        | _ -> failwith "Tega drevesa ne morem zavrteti"
+
+    let rec visina = function
+        | Prazno -> 0
+        | Sestavljeno (_, _, _, h) -> h
 
     let razlika = function
-      | Prazno -> 0
-      | Sestavljeno (l, _, d, _) -> visina l - visina d
+        | Prazno -> 0
+        | Sestavljeno (l, _, d, _) -> visina l - visina d
 
     let uravnotezi drevo =
-      match drevo with
-      | Prazno -> Prazno
-      | Sestavljeno (l, x, d, _)
-          when razlika drevo = 2 && razlika l = 1 ->
+        match drevo with
+        | Prazno -> Prazno
+        | Sestavljeno (l, x, d, _)
+            when razlika drevo = 2 && razlika l = 1 ->
             zavrti_desno drevo
-      | Sestavljeno (l, x, d, _)
-          when razlika drevo = 2  ->
+        | Sestavljeno (l, x, d, _)
+            when razlika drevo = 2  ->
             sestavljeno (zavrti_levo l, x, d) |> zavrti_desno
-      | Sestavljeno (l, x, d, _)
-          when razlika drevo = -2 && razlika d = -1 ->
+        | Sestavljeno (l, x, d, _)
+            when razlika drevo = -2 && razlika d = -1 ->
             zavrti_levo drevo
-      | Sestavljeno (l, x, d, _)
-          when razlika drevo = -2 ->
+        | Sestavljeno (l, x, d, _)
+            when razlika drevo = -2 ->
             sestavljeno (l, x, zavrti_desno d) |> zavrti_levo
-      | _ -> drevo
+        | _ -> drevo
 
     let rec dodaj x = function
-      | Prazno -> Sestavljeno (Prazno, x, Prazno, 0)
-      | Sestavljeno (l, y, d, h) as drevo ->
-          if x < y then
-            sestavljeno (dodaj x l, y, d) |> uravnotezi
-          else if y < x then
-            sestavljeno (l, y, dodaj x d) |> uravnotezi
-          else
-            drevo
+      | Prazno -> sestavljeno (Prazno, x, Prazno)
+      | Sestavljeno (l, y, d, _) when x < y -> sestavljeno (dodaj x l, y, d) |> uravnotezi
+      | Sestavljeno (l, y, d, _) when x > y -> sestavljeno (l, y, dodaj x d) |> uravnotezi
+      | drevo -> drevo
 end
 
 (* module M = MnozicaSeznami *)
