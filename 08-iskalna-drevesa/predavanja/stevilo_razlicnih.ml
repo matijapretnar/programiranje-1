87c82a9b4cd7fd68aa041ff53ebade2f3c0c6c29
Docker <docker@example.com>
2019-12-24 11:08:24 +0000

    Števila različnih na različne načine

--- 08-iskalna-drevesa/predavanja/stevilo_razlicnih.ml
+++ 08-iskalna-drevesa/predavanja/stevilo_razlicnih.ml
@@ -1,16 +1,19 @@
-Random.self_init ()
-
-;;
+let prazna = []
 
 let velikost m = List.length m
-let dodaj x m = if List.mem x m then m else x :: m
+
+let vsebuje x m = List.mem x m
+
+let dodaj x m = if vsebuje x m then m else x :: m
+
+(* ------------------------------------------------------------------------- *)
 
 let stevilo_razlicnih xs =
   let rec aux ze_videni = function
     | [] -> velikost ze_videni
     | x :: xs -> aux (dodaj x ze_videni) xs
   in
-  aux [] xs
+  aux prazna xs
 
 let nakljucni_seznam m n = List.init n (fun _ -> Random.int m)
 
@@ -27,4 +30,5 @@ let n = stopaj stevilo_razlicnih primer
 
 ;;
 
+Random.self_init ();
 print_endline ("Število različnih: " ^ string_of_int n)
