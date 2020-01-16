d0c124b7fd354f361beba9ead14bf9bc7eb23a42
Docker <docker@example.com>
2020-01-16 09:36:03 +0000

    Datoteke s predavanj

--- 13-memoizacija/predavanja/kvadrati.ml
+++ 13-memoizacija/predavanja/kvadrati.ml
@@ -1,10 +1,25 @@
-let kvadrati = Hashtbl.create 512
+let kvadrat x =
+  let y = x * x in
+  Format.printf "%d^2 = %d" x y;
+  y
 
-let mem_kvadrat x =
-  match Hashtbl.find_opt kvadrati x with
-  | Some y -> y
-  | None ->
-      print_endline ("Računam kvadrat števila " ^ string_of_int x);
-      let y = x * x in
-      Hashtbl.add kvadrati x y;
-      y
+let find_opt hashtbl x =
+    try
+        Some (Hashtbl.find hashtbl x)
+    with
+        Not_found -> None
+
+let memoiziraj f =
+  let rezultati = Hashtbl.create 512 in
+  let mem_f x =
+    match find_opt rezultati x with
+    | None ->
+        let y = f x in
+        Hashtbl.add rezultati x y;
+        y
+    | Some y ->
+        y
+  in
+  mem_f
+
+let mem_kvadrat = memoiziraj kvadrat
--- 13-memoizacija/predavanja/stolpi.ml
+++ 13-memoizacija/predavanja/stolpi.ml
@@ -1,32 +1,38 @@
 let rec rdeci = function
   | 0 -> 1
   | n when n < 0 -> 0
-  | n -> modri (n - 1) + modri (n - 2)
+  | n -> Format.printf "M%d-" n; modri (n - 1) + modri (n - 2)
 
 and modri = function
   | 0 -> 1
   | n when n < 0 -> 0
-  | n -> rdeci (n - 2) + rdeci (n - 3)
+  | n -> Format.printf "R%d-" n; rdeci (n - 2) + rdeci (n - 3)
 
 let stolpi = function
   | 0 -> 1
-  | n -> rdeci n + modri n
+  | n -> modri n + rdeci n
 
-let odviti_rdeci _ m = function
+let odviti_rdeci _ modri = function
   | 0 -> 1
   | n when n < 0 -> 0
-  | n -> m (n - 1) + m (n - 2)
+  | n -> Format.printf "R%d-" n; modri (n - 1) + modri (n - 2)
 
-let odviti_modri r _ = function
+let odviti_modri rdeci _ = function
   | 0 -> 1
   | n when n < 0 -> 0
-  | n -> r (n - 2) + r (n - 3)
+  | n -> Format.printf "M%d-" n; rdeci (n - 2) + rdeci (n - 3)
+
+let find_opt hashtbl x =
+    try
+        Some (Hashtbl.find hashtbl x)
+    with
+        Not_found -> None
 
 let memoiziraj_rec2 odviti_f odviti_g =
   let rezultati_f = Hashtbl.create 512 in
   let rezultati_g = Hashtbl.create 512 in
   let rec mem_f x =
-    match Hashtbl.find_opt rezultati_f x with
+    match find_opt rezultati_f x with
     | None ->
         let y = odviti_f mem_f mem_g x in
         Hashtbl.add rezultati_f x y;
@@ -34,7 +40,7 @@ let memoiziraj_rec2 odviti_f odviti_g =
     | Some y ->
         y
   and mem_g x =
-    match Hashtbl.find_opt rezultati_g x with
+    match find_opt rezultati_g x with
     | None ->
         let y = odviti_g mem_f mem_g x in
         Hashtbl.add rezultati_g x y;
@@ -42,9 +48,17 @@ let memoiziraj_rec2 odviti_f odviti_g =
     | Some y ->
         y
   in
-  mem_f, mem_g
+  (mem_f, mem_g)
 
 let mem_rdeci, mem_modri = memoiziraj_rec2 odviti_rdeci odviti_modri
-let mem_stolpi = function
+
+let mem_rdeci', mem_modri' = memoiziraj_rec2
+(fun _ modri -> function
   | 0 -> 1
-  | n -> mem_rdeci n + mem_modri n
+  | n when n < 0 -> 0
+  | n -> Format.printf "R%d-" n; modri (n - 1) + modri (n - 2)
+) (fun rdeci _ -> function
+  | 0 -> 1
+  | n when n < 0 -> 0
+  | n -> Format.printf "M%d-" n; rdeci (n - 2) + rdeci (n - 3)
+)
