6111badef9ae230e8e9d5b32080be93b7fdaf02a
Matija Pretnar <matija@pretnar.info>
2020-10-08 10:48:27 +0200

    Branje podatkov s knjižnico BS4

---
+++ 01-regularni-izrazi/predavanja/preberi_filme_bs4.py
@@ -0,0 +1,21 @@
+import bs4, re
+
+with open('250-najbolj-znanih-filmov.html') as f:
+    vsebina = f.read()
+
+zupa = bs4.BeautifulSoup(vsebina, 'html.parser')
+
+count = 0
+for povezava in zupa.find_all('a'):
+    link = povezava.get('href')
+    if link.endswith('?ref_=adv_li_tt'):
+        naslov = povezava.string
+        id = int(re.search('\d{7}', link).group(0))
+        znacka_z_letom = povezava.find_next_sibling()
+        niz_z_letom = znacka_z_letom.string
+        vzorec_leta = r'(\([IVXLCDM]+\) )?\((?P<leto>.*?)\)'
+        leto = int(re.fullmatch(vzorec_leta, niz_z_letom).group('leto'))
+        print(id, naslov, leto)
+        count += 1
+print(count)
+
