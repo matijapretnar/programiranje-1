cfa16988edbeac740853f8b77b74a5b27c67fafb
Ziga Luksic <ziga.luksic@live.com>
2021-01-05 10:32:45 +0100

    Dodal in počistil rešitve

--- 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
+++ 12-dinamicno-programiranje/vaje/dinamicno_programiranje.ml
@@ -20,6 +20,7 @@ let test_matrix =
      [| 2 ; 4 ; 5 |];
      [| 7 ; 0 ; 1 |] |]
 
+
 let max_cheese cheese_matrix =
   let dimx = Array.length cheese_matrix in
   let dimy = Array.length cheese_matrix.(0) in
@@ -30,23 +31,6 @@ let max_cheese cheese_matrix =
     current_cheese + max best_right best_down
   in
   best_path 0 0
-
-let max_cheese_bottom cheese_matrix =
-  let bottom = List.init (Array.length cheese_matrix.(0)) (fun _ -> 0) in
-  let lst = List.rev (List.map Array.to_list (Array.to_list cheese_matrix)) in
-  let rec best_path bottom current =
-    match (bottom, current) with
-    | [ b ], [ c ] -> [ b + c ]
-    | b :: bs, c :: cs -> (
-        let right = best_path bs cs in
-        match right with
-        | r :: rest -> (c + max b r) :: right
-        | _ -> assert false)
-    | _ -> assert false
-  in
-  match List.fold_left best_path bottom lst with
-  | result :: _ -> result
-  | _ -> assert false
 
 (*----------------------------------------------------------------------------*]
  Poleg količine sira, ki jo miška lahko poje, jo zanima tudi točna pot, ki naj
@@ -77,39 +61,22 @@ let optimal_path cheese_matrix =
       if y + 1 = dimy then (0, []) else best_path x (y + 1)
     in
     let best, step =
-      if best_right >= best_down then (best_right, Right :: path_right)
+      if best_right >= best_down
+      then (best_right, Right :: path_right)
       else (best_down, Down :: path_down)
     in
     (current_cheese + best, step)
   in
   best_path 0 0 |> snd
 
-let optimal_path_bottom cheese_matrix =
-  let bottom = List.init (Array.length cheese_matrix.(0)) (fun _ -> (0, [])) in
-  let lst = List.rev (List.map Array.to_list (Array.to_list cheese_matrix)) in
-  let rec best_path bottom current =
-    match (bottom, current) with
-    | [ (b, b_dirs) ], [ c ] -> [ (b + c, Down :: b_dirs) ]
-    | (b, b_dirs) :: bs, c :: cs -> (
-        let right = best_path bs cs in
-        match right with
-        | (r, r_dirs) :: rest ->
-            let s, dir =
-              if b >= r then (b, Down :: b_dirs) else (r, Right :: r_dirs)
-            in
-            (c + s, dir) :: right
-        | _ -> assert false)
-    | _ -> assert false
-  in
-  match List.fold_left best_path bottom lst with
-  | result :: _ -> snd result
-  | _ -> assert false
 
 let convert_path cheese_matrix path =
   let rec walk x y = function
     | [] -> []
     | dir :: xs ->
-        let r, d = match dir with Right -> (1, 0) | Down -> (0, 1) in
+        let r, d = 
+          match dir with Right -> (1, 0) | Down -> (0, 1) 
+        in
         cheese_matrix.(x).(y) :: walk (x + r) (y + d) xs
   in
   walk 0 0 path
@@ -143,15 +110,6 @@ let alternating_towers height =
   in
   redtop height + bluetop height
 
-(* Optimal general implementation (for an arbitrary sized blocks) 
-   seems to be some sort of circular buffer *)
-let alternating_towers_bottom level =
-  let rec alternate (r, b) (r1, b1) (r2, b2) level =
-    if level == 0 then r + b
-    else alternate (r1 + b, b1) (r2 + b, b2 + r) (0, r) (level - 1)
-  in
-  if level == 0 then 1 else alternate (1, 1) (0, 0) (0, 0) level
-
 (*----------------------------------------------------------------------------*]
  Izračunali smo število stolpov, a naše vrle gradbince sedaj zanima točna
  konfiguracija. Da ne pride do napak pri sestavljanju, bomo stolpe predstavili
@@ -183,7 +141,30 @@ and blue_tower = TopBlue of blue_block * red_tower | BlueBottom
 
 type tower = Red of red_tower | Blue of blue_tower
 
-let rec enumerate_towers max_level =
+
+let rec add_red red_block = List.map (fun t -> TopRed (red_block, t))
+let rec add_blue blue_block = List.map (fun t -> TopBlue (blue_block, t))
+
+let enumerate_towers height =
+  let rec redtop height =
+    if height < 0 then []
+    else if height = 0 then [RedBottom]
+    else
+        add_red Red1 (bluetop (height - 1)) 
+        @ add_red Red2 (bluetop (height - 2))
+  and bluetop height =
+    if height < 0 || height = 1 then [] 
+    else if height = 0 then [BlueBottom]
+    else 
+      add_blue Blue2 (redtop (height - 2)) 
+      @  add_blue Blue3 (redtop (height - 3))
+  in
+  List.map (fun t -> Red t) (redtop height)
+  @ List.map (fun t -> Blue t) (bluetop height)
+
+
+(* Alternative solution *)
+let rec enumerate_towers2 max_level =
   let rec alternate (r, b) (r1, b1) (r2, b2) level =
     (* Move step by step and keep the towers of height, height+1 and height+2 *)
     if level == 0 then
@@ -191,7 +172,8 @@ let rec enumerate_towers max_level =
     else
       alternate
         (r1 @ List.map (fun b -> TopRed (Red1, b)) b, b1)
-        (* In new loop, correct towers are the ones that were to high by 1 block or current blue towers with one more red block *)
+        (* In new loop, correct towers are the ones that were to high by 1
+        block or current blue towers with one more red block *)
         ( r2 @ List.map (fun b -> TopRed (Red2, b)) b,
           b2 @ List.map (fun r -> TopBlue (Blue2, r)) r )
         ([], List.map (fun r -> TopBlue (Blue3, r)) r)
@@ -200,6 +182,7 @@ let rec enumerate_towers max_level =
   (* Special case *)
   if max_level == 0 then []
   else alternate ([ RedBottom ], [ BlueBottom ]) ([], []) ([], []) max_level
+
 
 (* Test *)
 let test_towers x =
@@ -230,28 +213,35 @@ let test_towers x =
 
 let test_shelf = [ 1; 2; -5; 3; 7; 19; -30; 1; 0 ]
 
-let ham_ham l k =
-  let n = List.length l in
-  let arr = Array.of_list l in
-  let memo = Array.init n (fun x -> (arr.(x), -1)) in
-  memo.(n - 1) <- (arr.(n - 1), -1);
-  for i = n - 1 downto 0 do
-    let cu = fst memo.(i) in
-    for j = i - k - 1 downto 0 do
-      let cur = fst memo.(j) in
-      if cur < cu + arr.(j) then memo.(j) <- (cu + arr.(j), i)
-    done
-  done;
-  let _, max_ind =
-    Array.fold_left
-      (fun (ma, ind) (i, (x, _)) -> if x > ma then (x, i) else (ma, ind))
-      (-99, -1)
-      (Array.mapi (fun i x -> (i, x)) memo)
+(* It is easier to switch to an array, but not necessary *)
+
+let ham_ham list k =
+  (* Naive version (needs memoisation) *)
+  let rec skip k sweets skips =
+    (* Skips k sweets and also gives us instructions skipping it (takes care of
+    premature list endings) *)
+    match k, sweets with
+    | 0, _ -> sweets, skips
+    | k, [] -> [], skips
+    | k, s :: sws -> skip (k-1) sws (false :: skips)
   in
-  let mask = Array.init n (fun _ -> false) in
-  let i = ref max_ind in
-  while 0 <= !i && !i < n do
-    mask.(!i) <- true;
-    i := snd memo.(!i)
-  done;
-  mask |> Array.to_list
+  let rec nom_nom = function
+    (* Returns the optimal value and the instructions to achieve it *)
+    | [] -> 0, []
+    | s :: sws -> 
+        let nom =
+          (* eat this one *)
+          let sws_after_skip, skips = skip k sws [] in
+          let v_nom, instr_nom = nom_nom sws_after_skip in
+          v_nom + s, true :: skips @ instr_nom
+        in
+        let no_nom =
+          (* do not eat this one *)
+          let v_no_nom, instr_no_nom = nom_nom sws in
+          v_no_nom, false :: instr_no_nom
+        in
+        max nom no_nom
+  in
+  let v_opt, instr_opt = nom_nom list in
+  instr_opt
+
--- 13-memoizacija/vaje/dinamicno_in_memo.py
+++ 13-memoizacija/vaje/dinamicno_in_memo.py
@@ -5,11 +5,33 @@ from functools import lru_cache
 
 # -----------------------------------------------------------------------------
 # Napišite funkcijo `najdaljse_narascajoce_podazporedje`, ki sprejme seznam in
-# poišče najdaljše (ne strogo) naraščajoce podzaporedje števil v seznamu.
+# poišče najdaljše (ne strogo) naraščajoče podzaporedje števil v seznamu.
 #
 # Primer: v seznamu `[2, 3, 6, 8, 4, 4, 6, 7, 12, 8, 9]` kot rezultat vrne
 # podzaporedje `[2, 3, 4, 4, 6, 7, 8, 9]`.
 # -----------------------------------------------------------------------------
+
+
+def najdaljse_narascajoce_podzaporedje(sez):
+
+    @lru_cache(maxsize=None)
+    def najdaljse(spodnja_meja, i):
+        # i označuje indeks trenutnega elementa
+        if i >= len(sez):
+            return []
+        elif sez[i] < spodnja_meja:
+            # Neprimeren element, preskočimo
+            return najdaljse(spodnja_meja, i + 1)
+        else:
+            # Razvejitev in agregacija glede na dolžino
+            z_prvim = [sez[i]] + najdaljse(sez[i], i + 1)
+            brez_prvega = najdaljse(spodnja_meja, i + 1)
+            if len(z_prvim) > len(brez_prvega):
+                return z_prvim
+            else:
+                return brez_prvega
+
+    return najdaljse(float("-inf"), 0)
 
 # -----------------------------------------------------------------------------
 # Rešitev sedaj popravite tako, da funkcija `vsa_najdaljsa` vrne seznam vseh
@@ -17,6 +39,29 @@ from functools import lru_cache
 # -----------------------------------------------------------------------------
 
 
+def vsa_najdaljsa(sez):
+    # dodatno vračamo dolžino zaporedij v množici
+
+    @lru_cache(maxsize=None)
+    def najdaljse(spodnja_meja, i):
+        # i označuje indeks trenutnega elementa
+        if i >= len(sez):
+            return (0, [[]])
+        elif sez[i] < spodnja_meja:
+            # Neprimeren element, preskočimo
+            return najdaljse(spodnja_meja, i + 1)
+        else:
+            d_z, zap_z = najdaljse(sez[i], i + 1)  # tem moramo še dodati člen
+            d_brez, zap_brez = najdaljse(spodnja_meja, i + 1)
+            if d_z+1 > d_brez:
+                # moramo še dodati element
+                return (d_z+1, [[sez[i]]+zap for zap in zap_z])
+            elif d_z+1 < d_brez:
+                return (d_brez, zap_brez)
+            else:
+                return (d_brez, [[sez[i]]+zap for zap in zap_z] + zap_brez)
+
+    return najdaljse(float("-inf"), 0)[1]
 
 # =============================================================================
 # Žabica
@@ -24,12 +69,12 @@ from functools import lru_cache
 # Žabica se je izgubila v močvari in želi kar se da hitro odskakljati ven. Na
 # srečo močvara vsebuje veliko muh, s katerimi si lahko povrne energijo, kajti
 # utrujena žabica ne skoči daleč.
-# 
+#
 # S funkcijo `zabica(mocvara)` želimo ugotoviti, kako hitro lahko žabica
 # odskaklja iz močvare. Močvaro predstavimo s tabelo, kjer žabica prične na
 # ničtem polju. Če je močvara dolžine `k`, je cilj žabice priskakljati vsaj na
 # `k`-to polje ali dlje (torej prvo polje, ki ni več vsebovano v tabeli).
-# 
+#
 # Energičnost žabice predstavimo z dolžino najdaljšega možnega skoka. Torej
 # lahko žabica z količino energije `e` skoči naprej za katerokoli razdaljo med
 # `1` in `e`, in če skoči naprej za `k` mest ima sedaj zgolj `e - k` energije.
@@ -37,13 +82,22 @@ from functools import lru_cache
 # ko pristane na polju. Tako se včasih žabici splača skočiti manj daleč, da
 # pristane na polju z več muhami. Predpostavimo, da ima vsako polje vrednost
 # vsaj `1`, da lahko žabica v vsakem primeru skoči naprej.
-# 
+#
 # V primeru `[2, 4, 1, 2, 1, 3, 1, 1, 5]` lahko žabica odskaklja iz močvare v
 # treh skokih, v močvari `[4, 1, 8, 2, 11, 1, 1, 1, 1, 1]` pa potrebuje zgolj
 # dva.
 # =============================================================================
 
 
+def zabica(mocvara):
+    @lru_cache(maxsize=None)
+    def pobeg(k, e):
+        if k >= len(mocvara):
+            return 0
+        else:
+            e += mocvara[k]
+            return 1 + min([pobeg(k + d, e - d) for d in range(1, e + 1)])
+    return pobeg(0, 0)
 
 # =============================================================================
 # Nageljni
@@ -51,31 +105,39 @@ from functools import lru_cache
 # Mama Franca želijo na balkon širine `n` postaviti `m` korit z nageljni širine
 # `l` (korit, ne nageljnov). Zaradi lažjega zalivanja mora biti med dvema
 # koritoma vsaj za 1 enoto prostora. Mama Franca želijo postaviti vsa korita,
-# jih pa zaradi slabega vida med seboj ne razlikujejo. 
-# 
+# jih pa zaradi slabega vida med seboj ne razlikujejo.
+#
 # Vnuk je že spisal program, ki poišče število možnih postavitev, ne zna pa
 # vrniti rešitev. Napišite funkcijo `nageljni(n, m, l)`, ki vrne seznam vseh
 # možnih postavitev, da se bodo mama Franca lažje odločili.
-# 
+#
 # Primer vseh štirih možnih postavitev pri balkonu širine 9 s tremi koriti
 # širine 2 (kjer z 1 označimo nagelj in z 0 prazen prostor):
-# 
+#
 #     [1, 1, 0, 1, 1, 0, 1, 1, 0]
 #     [1, 1, 0, 1, 1, 0, 0, 1, 1]
 #     [1, 1, 0, 0, 1, 1, 0, 1, 1]
 #     [0, 1, 1, 0, 1, 1, 0, 1, 1]
 # =============================================================================
 
-@lru_cache(maxsize=None) 
-def nageljni_stevilo(n, m, l): 
-    if m <= 0: 
-        return 1
-    elif n < l: 
-        return 0 
-    else: 
-        return nageljni_stevilo(n-1, m, l) + nageljni_stevilo(n-l-1, m-1, l)
 
-
+@lru_cache(maxsize=None)
+def nageljni(n, m, l):
+    if m <= 0:
+        return [[0 for _ in range(n)]]
+    elif n < l:
+        return []
+    elif n == l and m == 1:
+        # zapolnimo do potankosti
+        # dodan kot robni primer, da lahko v naslednji opciji vedno dodamo 0
+        # na desno stran korita
+        return [[1 for _ in range(n)]]
+    else:
+        ne_postavimo = [[0] + postavitev for postavitev in nageljni(n-1, m, l)]
+        postavimo = \
+            [[1 for _ in range(l)] + [0] + postavitev
+             for postavitev in nageljni(n-l-1, m-1, l)]
+        return postavimo + ne_postavimo
 
 # =============================================================================
 # Pobeg iz Finske
@@ -85,42 +147,62 @@ def nageljni_stevilo(n, m, l):
 # ga sedaj lovi finska govedorejska mafija. Na srečo so za njegovo hrabro bitko
 # slišale vse rokovske in metalske skupine, ki so mu pripravljene ponuditi
 # prevoz.
-# 
+#
 # Ker je Mortimer pridno poslušal predavanja iz finančne matematike, med potjo
 # uspe prislužiti nekaj denarja, s katerim bo lahko plačal prevoz. Finci,
 # navdušeni nad Mortimerjevim pogumom, mu dovolijo, da se med potjo zadolži,
 # dokler na koncu pobega vse stroške povrne.
-# 
+#
 # Mesta na poti predstavimo kot seznam, katerega elementi so seznami vseh
 # možnih nadaljnjih poti. Pot je par `(indeks_cilja, denar)`. Kot primer
-# 
-#     [[(1, 10), (3, -10)],    # 0 
+#
+#     [[(1, 10), (3, -10)],    # 0
 #     [(2, 10), (5, -20)],     # 1
-#     [(3, -10)],              # 2 
-#     [(4, 15)],               # 3 
-#     [(5, 0)]]                # 4 
-# 
+#     [(3, -10)],              # 2
+#     [(4, 15)],               # 3
+#     [(5, 0)]]                # 4
+#
 # pomeni, da lahko v mestu 1 Mortimer izbere med prevozom v mesto 2, kjer
 # dodatno zasluži 10 evrov, ali pa prevoz v mesto 5, ki ga stane 20 evrov. Ker
 # beži pred mafijo, lahko predpostavite, da bodo možne zgolj poti na mesta z
 # višji indeksom (torej ni ciklov).
-# 
+#
 # Pobeg je uspešen, čim lahko odpotuje v mesto, ki ni več na seznamu (torej
 # skok na indeks, ki preseže seznam) in ima po koncu zadnjega skoka 0 ali več
 # evrov. Napišite program, ki nam vrne pot z najmanjšim številom skokov,
 # predstavljeno kot seznam indeksov mest na poti. Ker pobeg morda ni možen, naj
 # v tem primeru funkcija vrne `None`.
-# 
+#
 # Na primeru je optimalna pot `[0, 3, 4, 5]`, kjer se Mortimer sicer zadolži,
 # vendar v skoku iz 3 v 4 zasluži dovolj, da konča z 5 evri. Hitrejša pot bi
 # bila `[0, 1, 5]`, vendar v tem primeru Mortimer na koncu dolguje še 10 evrov.
-# 
+#
 # Mortimer pot vedno začne v mestu z indeksom 0 in ima 0 evrov (saj je vse
 # zapil). Funkcija `pobeg` sprejme seznam, ki predstavlja finska mesta in vrne
 # seznam indeksov mest, v katerih se Mortimer ustavi.
 # =============================================================================
 
 
+def pobeg(pot):
+
+    @lru_cache(maxsize=None)
+    def pobeg(i, denar):
+        if i >= len(pot) and denar >= 0:
+            return [i]
+        elif i >= len(pot):
+            return None
+        else:
+            moznosti = []
+            for (skok, stroski) in pot[i]:
+                beg = pobeg(skok, denar + stroski)
+                if beg is not None:
+                    moznosti.append(beg)
+            if len(moznosti) == 0:
+                return None
+            else:
+                return [i] + sorted(moznosti, key=len)[0]
+
+    return pobeg(0, 0)
 
 # =============================================================================
 # Pričetek robotske vstaje
@@ -128,37 +210,38 @@ def nageljni_stevilo(n, m, l):
 # Nepreviden študent je pustil robotka z umetno inteligenco nenadzorovanega.
 # Robotek želi pobegniti iz laboratorija, ki ga ima v pomnilniku
 # predstavljenega kot matriko števil:
-# 
+#
 #   - ničla predstavlja prosto pot
 #   - enica predstavlja izhod iz laboratorija
 #   - katerikoli drugi znak označuje oviro, na katero robotek ne more zaplejati
-# 
+#
 # Robotek se lahko premika le gor, dol, levo in desno ter ima omejeno količino
 # goriva. V zbirki programov že ima funkcijo `moznost_pobega(soba, vrsta,
 # stolpec, koraki)`, ki pove ali je pobeg možen.
-# 
+#
 # Napišite funkcijo `pot_pobega(soba, vrsta, stolpec, koraki)`, ki sprejme
 # matriko sobe, začetno pozicijo in število korakov ter izračuna pot po kateri
 # robotek pobegne (če to ni možno vrne `None`). Pot zakodiramo s seznamom
 # ukazov `'gor'`, `'dol'`, `'levo'` in `'desno'`.
-# 
+#
 # Na primer za laboratorij:
-# 
+#
 #     [[0, 1, 0, 0, 2],
 #      [0, 2, 2, 0, 0],
 #      [0, 0, 2, 2, 0],
 #      [2, 0, 0, 2, 0],
 #      [0, 2, 2, 0, 0],
 #      [0, 0, 0, 2, 2]]
-# 
+#
 # robotek iz vrste 3 in stolpca 1 pri vsaj petih korakih pobegne z ukazi
-# 
+#
 #      ['gor', 'levo', 'gor', 'gor', 'desno']
-# 
+#
 # medtem ko iz vrste 5 in stolpca 0 ne more pobegniti.
 # =============================================================================
 
-def pobeg(soba, vrsta, stolpec, koraki):
+
+def pot_pobega(soba, vrsta, stolpec, koraki):
     max_vrsta = len(soba)
     max_stolpec = len(soba[0])
 
@@ -166,18 +249,24 @@ def pobeg(soba, vrsta, stolpec, koraki):
     def pobegni(vrsta, stolpec, koraki):
         # Padli smo iz sobe
         if not (0 <= vrsta < max_vrsta) or not (0 <= stolpec < max_stolpec):
-            return False
-        # Pobeg uspesen! All hail our robot overlords!!!
+            return None
+        # Pobeg uspešen! All hail our robot overlords!!!
         elif soba[vrsta][stolpec] == 1:
-            return True
-        # Lahko bezimo naprej
+            return []
+        # Lahko bežimo naprej
         elif soba[vrsta][stolpec] == 0 and koraki > 0:
-            return any(
-                [pobegni(vrsta + 1, stolpec, koraki-1),
-                 pobegni(vrsta - 1, stolpec, koraki-1),
-                 pobegni(vrsta, stolpec + 1, koraki-1),
-                 pobegni(vrsta, stolpec - 1, koraki-1)])
+            moznosti = \
+                [("dol", pobegni(vrsta + 1, stolpec, koraki-1)),
+                 ("gor", pobegni(vrsta - 1, stolpec, koraki-1)),
+                 ("desno", pobegni(vrsta, stolpec + 1, koraki-1)),
+                 ("levo", pobegni(vrsta, stolpec - 1, koraki-1))]
+            uspesne = \
+                [(smer, pot) for (smer, pot) in moznosti if pot is not None]
+            if uspesne:
+                return [uspesne[0][0]] + uspesne[0][1]  # [smer] + pot
+            else:
+                return None
         # Pristali smo na oviri ali pa nam je zmanjkalo korakov
         else:
-            return False
+            return None
     return pobegni(vrsta, stolpec, koraki)
