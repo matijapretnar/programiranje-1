8c36b7222420f75326839bc79d42ecd0c4cde7c2
Filip Koprivec <koprivec.filip@gmail.com>
2023-01-16 16:39:55 +0100

    Popravki izpita

--- izpiti/2023-01-19/naloga2.ml
+++ izpiti/2023-01-19/naloga2.ml
@@ -1,84 +1,14 @@
-(* 
-  list_tree je drevo, ki je ali list z vrednostjo, 
-  ali pa vozlišče, ki vsebuje seznam poddreves tipa list_tree 
-*)
-
 type 'a list_tree = Leaf of 'a | Node of 'a list_tree list
 
-(* 
-  Napišite funkcijo map, ki sprejme funkcijo in drevo ter 
-  podano drevo preslika s podano funkcijo.
-*)
+(* 1. a) *)
 
-let rec map f = function
-  | Leaf x -> Leaf (f x)
-  | Node xs -> Node (List.map (map f) xs)
+(* 1. b) *)
 
-(* 
-  Napišite funkcijo count, ki sprejme drevo in vrne število listov v drevesu.
-  Za vse točke naj bo funkcija repno rekurzivna.
-*)
+(* 1. c) *)
 
-let count_tlrec tree = 
-  let rec count' acc = function
-    | Leaf _ -> acc + 1
-    | Node xs -> List.fold_left count' acc xs
-  in
-  count' 0 tree
+(* 1. d) *)
 
-let rec count = function
-  | Leaf _ -> 1
-  | Node xs -> xs |> List.map count |> List.fold_left (+) 0
-
-(*  
-  Napišite funkcijo `apply`, tipa:
-    ('a -> 'b) list_tree -> 'a list_tree -> 'b list_tree
-    
-  Funkcija sprejme dve drevesi in vrne novo drevo, kjer je vsak element
-  rezultat aplikacije funkcije na elementu na isti poziciji v drugem drevesu.
-  Predpostavite lahko, da sta drevesi popolnoma enake oblike.
-
-*)
-
-let rec apply t1 t2 =
-  match (t1, t2) with
-  | Leaf f, Leaf x -> Leaf (f x)
-  | Node fs, Node xs -> Node (List.map2 apply fs xs)
-  | _ -> invalid_arg "apply"
-
-(*
-Napišite funkcijo `combine`, tipa:
-  ('a -> 'b) list_tree -> ('c -> 'a) list_tree -> ('c -> 'b) list_tree
-Funkcija sprejme dve drevesi, katerih elementi so funkcije, in vrne novo drevo,
-kjer je vsak element kompozitum enakoležnih funkcij.
-Predpostavite lahko, da sta drevesi popolnoma enake oblike. 
-
-Za vsa (smiselna) drevesa mora veljati:
-
-map (combine t1 t2) t3 == (map t1 (map t2 t3))
-
-*)
-
-let rec combine t1 t2 = 
-  match (t1, t2) with
-  | Leaf f, Leaf g -> Leaf (fun x -> f (g x))
-  | Node fs, Node xs -> Node (List.map2 combine fs xs)
-  | _ -> invalid_arg "combine"
-
-let rec apply_smart t1 t2 d1 d2 =
-  match (t1, t2) with
-  | Leaf f, Leaf x -> Leaf (f x)
-  | Node fs, Node xs ->
-      let rec process fs xs =
-        match (fs, xs) with
-        | [], [] -> []
-        | [], x :: xs -> apply (map (fun _ -> d1) x) x :: process fs xs
-        | f :: fs, [] -> apply f (map (fun _ -> d2) f) :: process fs xs
-        | f :: fs, x :: xs -> apply_smart f x d1 d2 :: process fs xs
-      in
-      Node (process fs xs)
-  | _ -> invalid_arg "apply_smart"
-
+(* 1. e) *)
 
 let t1 = Node [Node [Leaf (fun x -> x)]; Leaf (fun x -> x * 2)]
 
@@ -86,6 +16,6 @@ let t2 = Node [Leaf 1; Leaf 2]
 
 let t3 = Node [Node []; Leaf 2; Leaf 4]
 
-let t4 = apply_smart t1 t3 (fun x -> x* 100) 0
+let t4 = Node [Node [Leaf (fun x -> x)]; Leaf (fun x -> x * 2)]
 
-let k = apply_smart ( Node [Node [Leaf (fun x -> x)]; Leaf (fun x -> x * 2)]) (Node [Node []; Leaf 2; Leaf 4]) (fun x -> x * 110) 19
+let t5 = Node [Node []; Leaf 2; Leaf 4]
--- izpiti/2023-01-19/naloga3.py
+++ izpiti/2023-01-19/naloga3.py
@@ -1,102 +1,11 @@
-"""
-Bacek Jon se nahaja pred gorskim grebenom, 
-ki ga mora preplezati in se pri tem čim bolj 
-najesti (trenutno še) živih zelišč, ki poraščajo 
-gorovje.
-
-Izgled grebena je predstavljen spodaj, kjer 
-bacek začne povsem spodaj levo na točki z 
-vrednostjo 1 in konča povsem desno na točki 
-z vrednostjo 5, pri tem pa se lahko premika 
-zgolj po nepraznih celicah (na posameznem 
-kosu zemlje je vedno nenegativno število zelišč), 
-kjer ga premik v desno/levo stane 10 enot energije, 
-vertikalni 12, diagonalni pa 14 enot energije. 
-V prvem delu se lahko bacek 
-premika samo desno, ali po pripadajoči 
-diagonali dessno navzgor, v drug polovici pa zgolj desno, 
-ali pa desno navzdol.
-
-    60  50  40  30  20  30  40  50  60  70
-  40  50  60  73  80      40  60  30  20  40
-10  20  30  40  50          10  20  90  40  50
-
-V pythonu (ali OCamlu) to predstavimo s parom seznamov
-
-[
- [60,50,40,30,20],
- [40,50,60,73,80],
- [10,20,30,40,50],
-]
-[
- [30,40,50,60,70],
- [40,60,30,20,40],
- [10,20,90,40,50],
-]
-
-Vaša naloga je, da pripravite program, ki bo za dani greben
-izračunal največjo količino energije, ki jo ima lahko bacek 
-ob koncu poti. Bacek ob premiku vsakič poje zelišča, ki so 
-na lokaciji in s tem pridobi enako količino energije.
-Predpostavite lahko, da bo neka pote čez greben vedno obstajala
-
-"""
-
-from functools import cache
-
-matrix = list[list[int]]
-
-def bacek_jon(up: matrix, down: matrix):
-    horizontal_cost = 10
-    vertical_cost = 12
-    diagonal_cost = 14
-    def solve(data: matrix, inverse: bool, initial_energy: int) -> int:
-        h = len(data)
-        w = len(data[0])
-        @cache
-        def solve(i : int, j: int, energy: int) -> tuple[list[str], int]:
-            energy += data[i][j]
-            # At the end
-            if i == h-1 and j == w-1:
-                return [], energy
-
-            right = None, (None, -1)
-            diagonal = None, (None, -1)
-            up = None, (None, -1)
-
-            # Can move right
-            if j < w-1:
-                right = "R", solve(i, j+1, energy - horizontal_cost)
-            # Can move diagonal
-            if i < h-1:
-                diagonal = "X", solve(i+1, j, energy - diagonal_cost)
-            if i < h - 2 and j > 0:
-                up = ("U" if inverse else "D" ), solve(i+2, j-1, energy - vertical_cost)
-            
-            dir, (prev, energy) = max([right, diagonal, up], key=lambda x: x[1][1])
-
-            return prev + [dir], energy
-        
-        return solve(0, 0, initial_energy)
-
-    fst = solve(up[::-1], True, 0)
-    snd = solve(down, False, fst[1] - horizontal_cost)
-
-    return snd[1] + fst[1], fst[0][::-1] + ["R"] + snd[0][::-1]
-
-
-
-print(
-    bacek_jon([
- [60,50,40,30,20],
- [40,50,60,73,80],
- [10,20,30,40,50],
-],
-[
- [30,40,50,60,70],
- [40,60,30,20,40],
- [10,20,90,40,50],
-]
-    )
-)
-
+breg1, breg2 = ([
+    [60,50,40,30,20],
+    [40,50,60,73,80],
+    [10,20,30,40,50],
+    ],
+    [
+    [30,40,50,60,70],
+    [40,60,30,20,40],
+    [10,20,90,40,50],
+    ]
+  )
