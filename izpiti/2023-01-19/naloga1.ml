23a7144f122d868d0a6b8b9cd0b17c3eea8dd967
Filip Koprivec <koprivec.filip@gmail.com>
2023-02-04 07:59:04 +0100

    Dodal prvi izpit

---
+++ izpiti/2023-01-19/naloga1.ml
@@ -0,0 +1,13 @@
+(* 1. a) *)
+
+(* 1. b) *)
+
+(* 1. c) *)
+
+(* 1. d) *)
+
+type response = Left | Middle | Right
+
+(* 1. e) *)
+
+type ('a, 'b) sum = Left of 'a | Right of 'b
---
+++ izpiti/2023-01-19/naloga2.ml
@@ -0,0 +1,15 @@
+type 'a list_tree = Leaf of 'a | Node of 'a list_tree list
+
+(* 1. a) *)
+
+(* 1. b) *)
+
+(* 1. c) *)
+
+(* 1. d) *)
+
+(* 1. e) *)
+
+let t1 = Node [ Node [ Leaf (fun x -> x) ]; Leaf (fun x -> x * 2) ]
+let t2 = Node [ Leaf 1; Leaf 2 ]
+let t3 = Node [ Node []; Leaf 2; Leaf 4 ]
