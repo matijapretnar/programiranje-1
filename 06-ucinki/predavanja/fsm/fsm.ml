f70422a5dcaa8be4a5d9e749e450b801fb97f779
Matija Pretnar <matija@pretnar.info>
2023-11-06 15:53:31 +0100

    Poenostavitev modela

--- 06-ucinki/predavanja/fsm/fsm.ml
+++ 06-ucinki/predavanja/fsm/fsm.ml
@@ -1,60 +1,41 @@
 open Model
 open Vdom
 
-let _random_fsm n m =
-  let states =
-    Array.init n (fun i ->
-        let name = string_of_int i in
-        { name })
+let ravno_prav_nicel =
+  let q0 = { name = "ostanek 0" }
+  and q1 = { name = "ostanek 1" }
+  and q2 = { name = "ostanek 2" } in
+  let transitions =
+    [
+      (q0, '0', q1);
+      (q1, '0', q2);
+      (q2, '0', q0);
+      (q0, '1', q0);
+      (q1, '1', q1);
+      (q2, '1', q2);
+    ]
   in
-  let initial = states.(0)
-  and final =
-    states |> Array.to_list |> List.filter (fun _ -> Random.float 1.0 < 0.3)
-  and transitions =
-    List.init m (fun _ ->
-        let src = Random.int n in
-        let dst = Random.int n in
-        let chr = char_of_int (Random.int 26 + int_of_char 'A') in
-        (states.(src), chr, states.(dst)))
-  in
-  { states; initial; final; transitions }
+  { states = [ q0; q1; q2 ]; initial = q0; final = [ q1 ]; transitions }
 
-let even_a =
-  let even = { name = "E" } and odd = { name = "O" } in
-  {
-    states = [| even; odd |];
-    initial = even;
-    final = [ even ];
-    transitions =
-      [ (even, 'A', odd); (even, 'B', even); (odd, 'A', even); (odd, 'B', odd) ];
-  }
-
-let init width height fsm string =
-  let n = Array.length fsm.states in
-  let pi = 4. *. atan 1. in
-  let origin = { x = width /. 2.; y = height /. 2. } in
-  let characters = string |> String.to_seq |> List.of_seq in
-  let positions =
-    Array.mapi
-      (fun i state ->
-        let angle = 2. *. pi *. float_of_int i /. float_of_int n
-        and r = (min width height /. 2.) -. (2. *. View.config.loop_radius) in
-        (state, origin ++. { x = r *. cos angle; y = r *. sin angle }))
-      fsm.states
-    |> Array.to_list
-  in
-  {
-    fsm;
-    positions;
-    mode = Normal;
-    width;
-    height;
-    characters;
-    current_state = fsm.initial;
-  }
+(* let vsebuje_samo_nicle =
+   let ima_enke = { name = "ima 1" } and nima_enk = { name = "nima 1" } in
+   {
+     states = [ ima_enke; nima_enk ];
+     initial = nima_enk;
+     final = [ nima_enk ];
+     transitions =
+       [
+         (nima_enk, '0', nima_enk);
+         (nima_enk, '1', ima_enke);
+         (ima_enke, '0', ima_enke);
+         (ima_enke, '1', ima_enke);
+       ];
+   } *)
 
 let app =
-  simple_app ~init:(init 500. 500. even_a "ABABA") ~view:View.view ~update ()
+  simple_app
+    ~init:(init 500. 500. ravno_prav_nicel "001010101011")
+    ~view:View.view ~update ()
 
 let () =
   let open Js_browser in
--- 06-ucinki/predavanja/fsm/model.ml
+++ 06-ucinki/predavanja/fsm/model.ml
@@ -40,6 +40,29 @@ type model = {
   current_state : state;
 }
 
+let init width height fsm string =
+  let n = List.length fsm.states in
+  let pi = 4. *. atan 1. in
+  let origin = { x = width /. 2.; y = height /. 2. } in
+  let characters = string |> String.to_seq |> List.of_seq in
+  let positions =
+    List.mapi
+      (fun i state ->
+        let angle = 2. *. pi *. float_of_int i /. float_of_int n
+        and r = min width height /. 2. *. 0.9 in
+        (state, origin ++. { x = r *. cos angle; y = r *. sin angle }))
+      fsm.states
+  in
+  {
+    fsm;
+    positions;
+    mode = Normal;
+    width;
+    height;
+    characters;
+    current_state = fsm.initial;
+  }
+
 type msg = DragStart of state | DragMove of vector | DragEnd | Next
 
 let state_position model state = List.assoc state model.positions
