0ffe04bb27d1d0c4bf2eda7d94b6fb92966d08af
Matija Pretnar <matija@pretnar.info>
2024-09-10 08:52:49 +0200

    Delne rešitve 3. izpita

--- izpiti/2024-08-28/naloga1.ml
+++ izpiti/2024-08-28/naloga1.ml
@@ -1,7 +1,84 @@
 (* 1. a *)
+(*----------------------------------------------------------------------------*]
+  Napišite predikat `obrnljiva : (int * int) * (int * int) -> bool`, ki za dano 
+  dvodimenzionalno matriko $A$ pove, ali je obrnljiva, torej da njena determinanta
+  ni enaka 0. (Determinanta matrike ((a, b), (c, d)) je enaka ad - bc.)
 
-(* 1. b *)
+  # obrnljiva ((3, 4), (0, 0));;
+  - : bool = false
+  # obrnljiva ((1, 0), (0, 1));;
+  - : bool = true
+[*----------------------------------------------------------------------------*)
 
-(* 1. c *)
+let obrnljiva ((a, b), (c, d)) = a * d - b * c <> 0
 
-(* 1. d *)
+(* b *)
+(*----------------------------------------------------------------------------*]
+  Napišite funkcijo `natanko_en : int option -> int option -> int option`, 
+  ki sprejme morebitna elementa ter vrne element natanko tedaj,
+  ko dobi ravno enega.
+  
+    # natanko_en (Some 1) (Some 2);;
+    - : int option = None
+    # natanko_en (Some 1) None;;
+    - : int option = Some 1
+    # natanko_en None (Some 2);;
+    - : int option = Some 2
+    # natanko_en None None;;
+    - : int option = None
+[*----------------------------------------------------------------------------*)
+
+let natanko_en a b = 
+  match a, b with
+  | Some _, Some _ -> None
+  | Some x, None -> Some x
+  | None, Some x -> Some x
+  | None, None -> None
+
+(* c *)
+(*----------------------------------------------------------------------------*]
+  Napišite funkcijo `aritmeticno : int list -> int option`, ki pove, ali je 
+  zaporedje števil aritmetično. Če je, vrne razliko, sicer pa `None`. 
+  Zaporedja z enim elementom ali manj niso aritmetična.
+
+    # aritmeticno [];;
+    - : int option = None
+    # aritmeticno [1; 5; 9];;
+    - : int option = Some 4
+    # aritmeticno [3; 4; 5; 2];;
+    - : int option = None
+[*----------------------------------------------------------------------------*)
+
+let aritmeticno = function
+  | [] | _ :: [] -> None
+  | x :: y :: zs -> 
+    let razlika = y - x in
+    let rec aux = function
+      | x :: y :: [] when y - x = razlika -> Some razlika
+      | x :: y :: zs when y - x = razlika -> aux (y :: zs)
+      | _ -> None
+    in
+    aux (y :: zs)
+
+(* d *)
+(*----------------------------------------------------------------------------*]
+  Napišite funkcijo `filtriraj : 'a list -> bool list -> 'a list`, ki sprejme
+  seznam elementov in seznam logičnih vrednosti. Vrne naj seznam tistih elementov
+  prvega seznama, pri katerih se v drugem seznamu na istem mestu nahaja `true`.
+  Funkcija naj ignorira logične vrednosti na koncu seznama, če jih je preveč.
+  Če je seznam logičnih vrednosti prekratek, naj se obnaša, kot da so manjkajoče 
+  vrednosti `true`.
+
+    # filtriraj [1; 2; 3; 4; 5] [true; false; true; false; true];;
+    - : int list = [1; 3; 5]
+    # filtriraj [1; 2] [false; true; false; true];;
+    - : int list = [2]
+    # filtriraj [1; 2; 3; 4; 5] [false];;
+    - : int list = [2; 3; 4; 5]
+[*----------------------------------------------------------------------------*)
+
+let rec filtriraj xs bs = 
+  match xs, bs with
+  | [], [] | [], _ -> []
+  | xs, [] -> xs
+  | x :: xs, b :: bs -> if b then x :: (filtriraj xs bs) else filtriraj xs bs
--- izpiti/2024-08-28/naloga2.ml
+++ izpiti/2024-08-28/naloga2.ml
@@ -1,3 +1,31 @@
+(*----------------------------------------------------------------------------*]
+    Imejmo preprost procesor, sestavljen iz treh komponent:
+    - tabele ukazov `instructions`
+    - kazalca na trenutni ukaz `instruction_pointer`
+    - pomnilnika, ki je predstavljen s tabelo celih števil `memory`
+
+    Mesta v pomnilniku so oštevilčena z naslovi #0, #1, …, ukazi procesorja pa so:
+    - `INC a`, ki za 1 poveča vrednost na mestu `a` v pomnilniku,
+    - `DEC a`, ki za 1 zmanjša vrednost na mestu `a` v pomnilniku,
+    - `COPY a1 a2`, ki na mesto `a2` shrani vrednost, shranjeno na mestu `a1`,
+    - `WRITE x a`, ki na mesto `a` shrani število `x`.
+    - `JMP ip`, ki kazalec ukaza nastavi na `ip`,
+    - `JMPZ a ip`, ki kazalec ukaza nastavi na `ip`,
+        če je na mestu `a` v pomnilniku shranjeno število 0,
+    Program se konča, ko kazalec ukaza skoči iz tabele ukazov.
+
+    Na primer, program
+      0 COPY #0 #2
+      1 COPY #1 #3
+      2 JMPZ #3 6
+      3 INC #2
+      4 DEC #3
+      5 JMP 2
+    v #2 zapiše vsoto števil na mestih #0 in #1 tako, da najprej #0 prekopira v #2,
+    #1 pa v #3, nato pa toliko časa povečuje #2 in zmanjšuje #3,
+    dokler #2 ne doseže 0.
+  [*----------------------------------------------------------------------------*)
+
 type address = Address of int
 
 type instruction =
@@ -51,10 +79,65 @@ let print_state state =
   print_endline ("IP: " ^ string_of_int state.instruction_pointer);
   print_memory state.memory
 
-(* 2. a *)
+(*----------------------------------------------------------------------------*]
+    Sestavite funkcijo `increase_instruction_pointer : state -> state`,
+    ki za 1 poveča kazalec ukaza.
+  [*----------------------------------------------------------------------------*)
+let increase_instruction_pointer state =
+  { state with instruction_pointer = state.instruction_pointer + 1 }
 
-(* 2. b *)
+(*----------------------------------------------------------------------------*]
+    Sestavite funkciji `read_memory : state -> address -> int`, ki prebere
+    vrednost, shranjeno na danem mestu pomnilnika, ter
+    `write_memory : int -> address -> state -> state`, ki na dano mesto v
+    pomnilniku zapiše dano vrednost. Tabelo prvotnega stanja naj funkcija pusti
+    nespremenjeno, vrne pa naj stanje s posodobljeno kopijo prvotne tabele.
+  [*----------------------------------------------------------------------------*)
 
-(* 2. c *)
+let read_memory state (Address a) = state.memory.(a)
 
-(* 2. d *)
+let write_memory i (Address a) state =
+  let memory = Array.copy state.memory in
+  memory.(a) <- i;
+  { state with memory }
+
+(*----------------------------------------------------------------------------*]
+    Sestavite funkcijo `step : state -> instruction -> state`, ki na danem stanju
+    izvede dani ukaz ter vrne novo stanje. Pri tem ne pozabite, da z izjemo skokov
+    po vsakem izvedenem ukazu povečamo kazalec ukaza. Tudi tu tabelo prvotnega
+    stanja pustite nespremenjeno in v končnem stanju vrnite njeno ustrezno
+    posodobljeno kopijo.
+  [*----------------------------------------------------------------------------*)
+
+let step state = function
+  | INC a ->
+      let i = read_memory state a in
+      state |> write_memory (i + 1) a |> increase_instruction_pointer
+  | DEC a ->
+      let i = read_memory state a in
+      state |> write_memory (i - 1) a |> increase_instruction_pointer
+  | COPY (addr1, addr2) ->
+      let i = read_memory state addr1 in
+      state |> write_memory i addr2 |> increase_instruction_pointer
+  | WRITE (i, a) -> state |> write_memory i a |> increase_instruction_pointer
+  | JMP ip -> { state with instruction_pointer = ip }
+  | JMPZ (a, ip) ->
+      let i = read_memory state a in
+      if i = 0 then { state with instruction_pointer = ip }
+      else increase_instruction_pointer state
+
+(*----------------------------------------------------------------------------*]
+    Sestavite funkcijo `run : state -> int array`, ki na procesorju v danem stanju
+    toliko časa izvaja ukaze, dokler kazalec ukazov ne skoči iz tabele. Ko se to
+    zgodi, vrnite končno stanje pomnilnika.
+  [*----------------------------------------------------------------------------*)
+let rec run state =
+  print_state state;
+  print_endline "------------";
+  let ip = state.instruction_pointer in
+  if ip < Array.length state.instructions then
+    let state' = step state state.instructions.(ip) in
+    run state'
+  else state.memory
+
+let _ = example |> run |> print_memory
