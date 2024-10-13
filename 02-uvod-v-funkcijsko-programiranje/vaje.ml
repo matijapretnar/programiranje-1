563fcef635626a6229f5f57b6fec9f6de2e4c063
j00sko <koprivec.filip@gmail.com>
2024-10-14 00:10:11 +0200

    Dodal vaje

---
+++ 02-uvod-v-funkcijsko-programiranje/vaje.ml
@@ -0,0 +1,132 @@
+(* ========== Vaja 2: Uvod v funkcijsko programiranje  ========== *)
+
+(*----------------------------------------------------------------------------*]
+Vektorje predstavimo kot seznam števil s plavajočo vejico.
+[*----------------------------------------------------------------------------*)
+
+type vector = float list
+
+(*----------------------------------------------------------------------------*]
+Definirajte enotske vektorje `i`, `j` in `k` v treh dimenzijah.
+[*----------------------------------------------------------------------------*)
+
+
+(*----------------------------------------------------------------------------*]
+Napišite funkcijo `razteg : float -> vector -> vector`, ki vektor, 
+predstavljen s seznamom števil s plavajočo vejico, pomnoži z danim skalarjem.
+[*----------------------------------------------------------------------------*)
+
+let rec razteg = ()
+
+(*----------------------------------------------------------------------------*]
+Napišite funkcijo `sestej : vector -> vector -> vector`, ki vrne vsoto dveh 
+vektorjev.
+[*----------------------------------------------------------------------------*)
+
+let rec sestej = ()
+
+(*----------------------------------------------------------------------------*]
+Napišite funkcijo `skalarni_produkt : vector -> vector -> float`, ki izračuna 
+skalarni produkt dveh vektorjev
+[*----------------------------------------------------------------------------*)
+
+let rec skalarni_produkt = ()
+
+(*----------------------------------------------------------------------------*]
+Napišite funkcijo `norma : vector -> float`, ki vrne evklidsko normo vektorja.
+[*----------------------------------------------------------------------------*)
+
+let rec norma = ()
+
+(*----------------------------------------------------------------------------*]
+Napišite funkcijo `projeciraj : vector -> vector -> vector`, ki izračuna 
+projekcijo prvega vektorja na drugega.
+[*----------------------------------------------------------------------------*)
+
+let rec projeciraj = ()
+
+(*----------------------------------------------------------------------------*]
+Napišite funkcijo `ovij : string -> string -> string`, ki sprejme ime HTML 
+oznake in vsebino ter vrne niz, ki predstavlja ustrezno HTML oznako.
+
+Primer:
+`ovij "h1" "Hello, world!"`
+
+[*----------------------------------------------------------------------------*)
+
+let rec ovij = ()
+
+(*----------------------------------------------------------------------------*]
+Napišite funkcijo `zamakni : int -> string -> string`, ki sprejme število 
+presledkov in niz ter vrne niz, v katerem je vsaka vrstica zamaknjena za ustrezno število presledkov.
+
+Primer:
+`zamakni 4 "Hello, world!"`
+
+[*----------------------------------------------------------------------------*)
+
+let rec zamakni = ()
+
+(*----------------------------------------------------------------------------*]
+Napišite funkcijo `ul : string list -> string`, ki sprejme seznam nizov in vrne 
+niz, ki predstavlja ustrezno zamaknjen neurejeni seznam v HTML-ju:
+
+Primer:
+`ul ["ananas"; "banana"; "čokolada"]`
+
+[*----------------------------------------------------------------------------*)
+
+let rec ul = ()
+
+(*----------------------------------------------------------------------------*]
+Napišite funkcijo `razdeli_vrstico : string -> string * string`, ki sprejme niz, 
+ki vsebuje vejico, loči na del pred in del za njo.
+
+Primer:
+`razdeli_vrstico "mleko, 2"`
+
+[*----------------------------------------------------------------------------*)
+
+let rec razdeli_vrstico = ()
+
+(*----------------------------------------------------------------------------*]
+Napišite funkcijo `pretvori_v_seznam_parov : string -> (string * string) list`, 
+ki sprejme večvrstični niz, kjer je vsaka vrstica niz oblike 
+"izdelek, vrednost", in vrne seznam ustreznih parov.
+
+Primer:
+`pretvori_v_seznam_parov "mleko, 2\nkruh, 1\njabolko, 5"`
+
+[*----------------------------------------------------------------------------*)
+
+let rec pretvori_v_seznam_parov = ()
+
+(*----------------------------------------------------------------------------*]
+Napišite funkcijo `pretvori_druge_komponente : ('a -> 'b) -> (string * 'a) list -> (string * 'b) list`,
+ki dano funkcijo uporabi na vseh drugih komponentah elementov seznama.
+
+Primer:
+```ml
+let seznam = [("ata", "mama"); ("teta", "stric")] in 
+pretvori_druge_komponente String.length seznam
+```
+
+[*----------------------------------------------------------------------------*)
+
+let rec pretvori_druge_komponente = ()
+
+(*----------------------------------------------------------------------------*]
+Napišite funkcijo `izracunaj_skupni_znesek : string -> string -> float`, ki 
+sprejme večvrstična niza nakupovalnega seznama in cenika in izračuna skupni 
+znesek nakupa.
+
+Primer:
+```ml
+let nakupovalni_seznam = "mleko, 2\njabolka, 5"
+and cenik = "jabolka, 0.5\nkruh, 2\nmleko, 1.5" in
+izracunaj_skupni_znesek cenik nakupovalni_seznam
+```
+
+[*----------------------------------------------------------------------------*)
+
+let rec izracunaj_skupni_znesek = ()
