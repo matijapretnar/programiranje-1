e244295042012127acc81eae95590e17dc64c736
j00sko <koprivec.filip@gmail.com>
2024-12-16 10:30:47 +0100

    vaje racunska zahtevnost

---
+++ 10-racunska-zahtevnost/vaje.md
@@ -0,0 +1,16 @@
+Ponovimo definicijo velikega O:
+$$
+f \in O(g) \iff \exists x_0 \exists M. \forall x > x_0. f(x) \leq M * g(x)
+$$
+
+1. $ h \in O(f) \implies a * h \in O(f)$
+
+2. $h \in O(f) \implies h \in O(f + g)$ za vsak $g$.
+
+3. $f \in O(g_1) \land f \in O(g_2) \implies f_1 + f_2 \in O(g_1 + g_2)$
+
+4. $h \in O(f_1 + f_2) \implies h \in O(max(f_1, f_2))$
+
+5. $O(log_b(n)) = O(log_2(n))$
+
+6. $O(n!) = O(n * \log(n))$ (Za $n > 10$)
---
+++ 10-racunska-zahtevnost/vaje.ml
@@ -0,0 +1,116 @@
+(* 
+Natančno definirajte pogoje, da funkcija `f` uredi seznam. 
+*)
+
+(*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*]
+ Urejanje z Vstavljanjem
+[*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*)
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [insert y xs] vstavi [y] v že urejen seznam [xs] in vrne urejen
+ seznam. 
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # insert 9 [0; 2];;
+ - : int list = [0; 2; 9]
+ # insert 1 [4; 5];;
+ - : int list = [1; 4; 5]
+ # insert 7 [];;
+ - : int list = [7]
+[*----------------------------------------------------------------------------*)
+
+
+(*----------------------------------------------------------------------------*]
+ Prazen seznam je že urejen. Funkcija [insert_sort] uredi seznam tako da
+ zaporedoma vstavlja vse elemente seznama v prazen seznam.
+[*----------------------------------------------------------------------------*)
+
+
+
+(*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*]
+ Urejanje z Izbiranjem
+[*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*)
+
+
+(*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
+ Pri urejanju z izbiranjem na vsakem koraku ločimo dva podseznama, kjer je prvi
+ že urejen, drugi pa vsebuje vse elemente, ki jih je še potrebno urediti. Nato
+ zaporedoma prenašamo najmanjši element neurejenega podseznama v urejen
+ podseznam, dokler ne uredimo vseh. 
+
+ Če pričnemo z praznim urejenim podseznamom, vemo, da so na vsakem koraku vsi
+ elementi neurejenega podseznama večji ali enaki elementom urejenega podseznama,
+ saj vedno prenesemo najmanjšega. Tako vemo, da moramo naslednji najmanjši člen
+ dodati na konec urejenega podseznama.
+ (Hitreje je obrniti vrstni red seznama kot na vsakem koraku uporabiti [@].)
+[*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*)
+
+
+(*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*]
+ Urejanje z Izbiranjem na Tabelah
+[*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*)
+
+(*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
+ Pri delu z tabelami (array) namesto seznami, lahko urejanje z izbiranjem 
+ naredimo "na mestu", t.j. brez uporabe vmesnih kopij (delov) vhoda. Kot prej
+ tabelo ločujemo na že urejen del in še neurejen del, le da tokrat vse elemente
+ hranimo v vhodni tabeli, mejo med deloma pa hranimo v spremenljivki
+ [boundary_sorted]. Na vsakem koraku tako ne izvlečemo najmanjšega elementa
+ neurejenga dela tabele temveč poiščemo njegov indeks in ga zamenjamo z
+ elementom na meji med deloma (in s tem dodamo na konec urejenega dela).
+ Postopek končamo, ko meja doseže konec tabele.
+[*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*)
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [swap a i j] zamenja elementa [a.(i)] and [a.(j)]. Zamenjavo naredi
+ na mestu in vrne unit.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # let test = [|0; 1; 2; 3; 4|];;
+ val test : int array = [|0; 1; 2; 3; 4|]
+ # swap test 1 4;;
+ - : unit = ()
+ # test;;
+ - : int array = [|0; 4; 2; 3; 1|]
+[*----------------------------------------------------------------------------*)
+
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [index_min a lower upper] poišče indeks najmanjšega elementa tabele
+ [a] med indeksoma [lower] and [upper] (oba indeksa sta vključena).
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ index_min [|0; 2; 9; 3; 6|] 2 4 = 4
+[*----------------------------------------------------------------------------*)
+
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [selection_sort_array] implementira urejanje z izbiranjem na mestu. 
+[*----------------------------------------------------------------------------*)
+
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [min_and_rest list] vrne par [Some (z, list')] tako da je [z]
+ najmanjši element v [list] in seznam [list'] enak [list] z odstranjeno prvo
+ pojavitvijo elementa [z]. V primeru praznega seznama vrne [None]. 
+[*----------------------------------------------------------------------------*)
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [selection_sort] je implementacija zgoraj opisanega algoritma.
+ Namig: Uporabi [min_and_rest] iz prejšnje naloge.
+[*----------------------------------------------------------------------------*)
+
+
+(*----------------------------------------------------------------------------*]
+ Funkcija [randlist len max] generira seznam dolžine [len] z naključnimi
+ celimi števili med 0 in [max].
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ # let l = randlist 10 10 ;;
+ val l : int list = [0; 1; 0; 4; 0; 9; 1; 2; 5; 4]
+[*----------------------------------------------------------------------------*)
+
+
+(*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*]
+ Sedaj lahko s pomočjo [randlist] primerjamo našo urejevalno funkcijo (imenovana
+ [our_sort] v spodnjem primeru) z urejevalno funkcijo modula [List]. Prav tako
+ lahko na manjšem seznamu preverimo v čem je problem.
+ - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+ let test = (randlist 100 100) in (our_sort test = List.sort compare test);;
+[*-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=*)
