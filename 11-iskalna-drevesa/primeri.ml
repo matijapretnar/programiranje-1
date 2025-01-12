e49306fa93cd903878d096d5a8c4857887e9ca47
Matija Pretnar <matija@pretnar.info>
2025-01-12 19:46:39 +0100

    Datoteke s predavanj

--- 11-iskalna-drevesa/primeri.ml
+++ 11-iskalna-drevesa/primeri.ml
@@ -118,7 +118,76 @@ module MnozicaPrekAVLDreves : MNOZICA = struct
         else drevo
 end
 
-module Mnozica = MnozicaPrekAVLDreves
+module MnozicaPrekPravilnoImplementiranihAVLDreves : MNOZICA = struct
+  type 'a t = Prazno | Sestavljeno of int * 'a t * 'a * 'a t
+
+  let rec vsebuje mn x =
+    match mn with
+    | Prazno -> false
+    | Sestavljeno (_, l, y, d) when x = y -> true
+    | Sestavljeno (_, l, y, d) when x < y -> vsebuje l x
+    | Sestavljeno (_, l, y, d) when x > y -> vsebuje d x
+    | _ -> assert false
+
+  let prazna = Prazno
+
+  let rec velikost = function
+    | Prazno -> 0
+    | Sestavljeno (_, l, _, d) -> 1 + velikost l + velikost d
+
+  let visina drevo =
+    match drevo with
+    | Prazno -> 0
+    | Sestavljeno (h, _, _, _) -> h
+
+  let sestavljeno (l, x, d) =
+    Sestavljeno (1 + max (visina l) (visina d), l, x, d)
+
+  let zavrti_levo = function
+    | Sestavljeno (_, l, x, Sestavljeno (_, dl, y, dd)) ->
+        sestavljeno (sestavljeno (l, x, dl), y, dd)
+    | _ -> failwith "Tega drevesa ne morem zavrteti"
+
+  let zavrti_desno = function
+    | Sestavljeno (_, Sestavljeno (_, ll, y, ld), x, d) ->
+        sestavljeno (ll, y, sestavljeno (ld, x, d))
+    | _ -> failwith "Tega drevesa ne morem zavrteti"
+
+  let razlika = function
+    | Prazno -> 0
+    | Sestavljeno (_, l, _, d) -> visina l - visina d
+
+  let uravnotezi drevo =
+    match drevo with
+    | Sestavljeno (_, l, x, d) when razlika drevo = 2 && razlika l = 1 ->
+        zavrti_desno drevo
+    | Sestavljeno (_, l, x, d) when razlika drevo = 2 ->
+        sestavljeno (zavrti_levo l, x, d) |> zavrti_desno
+    | Sestavljeno (_, l, x, d) when razlika drevo = -2 && razlika d = -1 ->
+        zavrti_levo drevo
+    | Sestavljeno (_, l, x, d) when razlika drevo = -2 ->
+        sestavljeno (l, x, zavrti_desno d) |> zavrti_levo
+    | _ -> drevo
+
+  let rec isci x drevo =
+    match drevo with
+    | Prazno -> false
+    | Sestavljeno (_, l, vrednost, d) ->
+        if x < vrednost then isci x l
+        else if x > vrednost then isci x d
+        else true
+
+  let rec dodaj x drevo =
+    match drevo with
+    | Prazno -> Sestavljeno (1, Prazno, x, Prazno)
+    | Sestavljeno (h, l, vrednost, d) ->
+        if x < vrednost then sestavljeno (dodaj x l, vrednost, d) |> uravnotezi
+        else if x > vrednost then
+          sestavljeno (l, vrednost, dodaj x d) |> uravnotezi
+        else drevo
+end
+
+module Mnozica = MnozicaPrekPravilnoImplementiranihAVLDreves
 
 let stevilo_razlicnih xs =
   let rec aux ze_videni = function
