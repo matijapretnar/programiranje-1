6feed6fff717c78be28afcb9f7b1b1b01e99f0ba
Matija Pretnar <matija@pretnar.info>
2025-10-07 09:06:21 +0200

    Pobrisal zastarele datoteke

--- projekt/src/definicije/stanje.mli
+++
@@ -1,4 +0,0 @@
-type t
-
-val iz_niza : string -> t
-val v_niz : t -> string
--- projekt/src/definicije/zagnaniAvtomat.mli
+++
@@ -1,8 +0,0 @@
-type t
-
-val pozeni : Avtomat.t -> Trak.t -> t
-val avtomat : t -> Avtomat.t
-val trak : t -> Trak.t
-val stanje : t -> Stanje.t
-val korak_naprej : t -> t option
-val je_v_sprejemnem_stanju : t -> bool
