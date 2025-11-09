3e41dc73994899837bcf82447c3511d5374037b3
Nejc Zajc <nz44156@student.uni-lj.si>
2025-11-09 23:02:40 +0100

    Lean vaje: Dockerfile in naloge

--- 06-dokazovalnik-lean/vaje/vaje-resitve.lean
+++ 06-dokazovalnik-lean/vaje/vaje-resitve.lean
@@ -1,3 +1,27 @@
+-- Strukture:
+
+-- (A x B) ^ C <=> A ^ C x B ^ C
+def eksponent (A B C : Type) (f : C → Prod A B) : Prod (C → A) (C → B) :=
+  ⟨
+    (fun (x : C) => (f x).1),
+    (fun (y : C) => (f y).2)
+  ⟩
+theorem eksponent_prop (A B C : Prop) (f : C → A ∧ B) : (C → A) ∧ (C → B) :=
+  ⟨
+    (fun (x : C) => (f x).1),
+    (fun (y : C) => (f y).2)
+  ⟩
+theorem eksponent_prop_s_taktikami (A B C : Prop) (f : C → A ∧ B) : (C → A) ∧ (C → B) :=
+  by
+    constructor
+    · intro h -- s pomočjo And.left
+      apply And.left
+      apply f
+      exact h
+    · intro h -- bolj neposredno
+      exact (f h).right
+
+-- ------------------------------
 -- LOGIKA
 
 -- (A ∧ B) ↔ (B ∧ A)
@@ -136,3 +160,37 @@ theorem eq7 {A B C : Prop} : C → (A ∧ B) ↔ (C → A) ∧ (C → B) :=
     constructor
     exact h.1 c
     exact h.2 c
+
+
+-- ------------------------------
+-- Enakosti naravnih števil (z uporabo `calc`)
+
+theorem kvadrat_dvoclenika {a b : Nat} : (a + b)^2 = a^2 + 2 * a * b + b^2 :=
+  by
+    calc
+      (a + b)^2
+      _ = (a + b) * (a + b) := by rw [Nat.pow_two]
+      _ = (a + b) * a + (a + b) * b := by rw [Nat.mul_add]
+      _ = a * a + b * a + (a + b) * b := by rw [Nat.add_mul]
+      _ = a * a + b * a + (a * b + b * b) := by rw [Nat.add_mul]
+      _ = a^2 + b * a + (a * b + b^2) := by rw [← Nat.pow_two, ← Nat.pow_two]
+      _ = a^2 + (b * a + (a * b + b^2)) := by rw [Nat.add_assoc]
+      _ = a^2 + ((b * a + a * b) + b^2) := by rw [Nat.add_assoc]
+      _ = a^2 + ((a * b + a * b) + b^2) := by rw [Nat.mul_comm b a]
+      _ = a^2 + (2 * (a * b) + b^2) := by rw [← Nat.two_mul]
+      _ = a^2 + 2 * (a * b) + b^2 := by rw [← Nat.add_assoc]
+      _ = a^2 + 2 * a * b + b^2 := by rw [Nat.mul_assoc]
+
+
+theorem vsota_eksponent_produkta {a b c d : Nat} : (a * b)^(c + d) = (a^c)*(a^d)*(b^c)*(b^d) :=
+  by
+    calc
+      (a * b)^(c + d)
+      _ = (a * b)^c * (a * b)^d := by rw [Nat.pow_add]
+      _ = (a^c * b^c) * (a * b)^d := by rw [Nat.mul_pow]
+      _ = (a^c * b^c) * (a^d * b^d) := by rw [Nat.mul_pow]
+      _ = ((a^c * b^c) * a^d) * b^d := by rw [← Nat.mul_assoc]
+      _ = (a^c * (b^c * a^d)) * b^d := by rw [← Nat.mul_assoc]
+      _ = (a^c * (a^d * b^c)) * b^d := by rw [Nat.mul_comm (b^c) (a^d)]
+      _ = ((a^c * a^d) * b^c) * b^d := by rw [← Nat.mul_assoc]
+      _ = a^c * a^d * b^c * b^d := by rfl
