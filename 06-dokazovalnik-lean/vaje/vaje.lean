af982652512321e02c40b4f8a9ad0b975ede0179
Nejc Zajc <nz44156@student.uni-lj.si>
2025-11-09 23:02:40 +0100

    Lean vaje: Dockerfile in naloge

---
+++ 06-dokazovalnik-lean/vaje/vaje.lean
@@ -0,0 +1,58 @@
+-- Strukture:
+
+-- (A x B) ^ C <=> A ^ C x B ^ C
+def eksponent (A B C : Type) (f : C → Prod A B) : Prod (C → A) (C → B) :=
+  ⟨
+    sorry,
+    sorry
+  ⟩
+def eksponent_prop (A B C : Prop) (f : C → A ∧ B) : (C → A) ∧ (C → B) :=
+  ⟨
+    sorry,
+    sorry
+  ⟩
+def eksponent_prop_s_taktikami (A B C : Prop) (f : C → A ∧ B) : (C → A) ∧ (C → B) :=
+  by
+    sorry
+
+
+-- ------------------------------
+-- Logika
+
+theorem eq1 {A B : Prop} : (A ∧ B) ↔ (B ∧ A) := by
+  sorry
+
+theorem eq2 {A B : Prop} : (A ∨ B) ↔ (B ∨ A) :=
+  sorry
+
+theorem eq3 {A B C : Prop} : (A ∧ (B ∧ C)) ↔ (B ∧ (A ∧ C)) :=
+  sorry
+
+theorem eq4 {A B C : Prop} : (A ∨ (B ∨ C)) ↔ (B ∨ (A ∨ C)) :=
+ sorry
+
+theorem eq5 {A B C : Prop} : A ∧ (B ∨ C) ↔ (A ∧ B) ∨ (A ∧ C) :=
+  sorry
+
+theorem eq6 {A B C : Prop} : (B ∨ C) → A ↔ (B → A) ∧ (C → A) :=
+  sorry
+
+theorem eq7 {A B C : Prop} : C → (A ∧ B) ↔ (C → A) ∧ (C → B) :=
+  sorry
+
+
+-- ------------------------------
+-- Enakosti naravnih števil (z uporabo `calc`)
+
+theorem kvadrat_dvoclenika {a b : Nat} : (a + b)^2 = a^2 + 2 * a * b + b^2 :=
+  by
+    calc
+      (a + b)^2
+      _ = a^2 + 2 * a * b + b^2 := by sorry
+
+
+theorem vsota_eksponent_produkta {a b c d : Nat} : (a * b)^(c + d) = (a^c)*(a^d)*(b^c)*(b^d) :=
+  by
+    calc
+      (a * b)^(c + d)
+      _ = a^c * a^d * b^c * b^d := by sorry
